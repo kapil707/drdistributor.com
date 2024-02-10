518040f391fba7b123986add6f105b0f39a60717
kapil-asus-new <kapil707sharma@gmail.com>
2024-02-10 10:16:49 +0530

    add

--- application/controllers/Chemist_json_test.php
+++ application/controllers/Chemist_json_test.php
@@ -26,11 +26,12 @@ class Chemist_json_test extends CI_Controller {
 		$user_altercode = $_COOKIE["user_altercode"];
 		$user_password	= $_COOKIE["user_password"];
 
-		$chemist_id 	= $_COOKIE["chemist_id"];
+		$chemist_id 	= "";
 
 		$salesman_id = "";
 		if($user_type=="sales")
 		{
+			$chemist_id 	= $_COOKIE["chemist_id"];
 			$salesman_id 	= $user_altercode;
 			$user_altercode = $chemist_id;
 		}

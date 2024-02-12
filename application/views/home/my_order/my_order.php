a5bc882d0435ddce9b7172ba4f2bf460a97c7cb3
kapil-asus-new <kapil707sharma@gmail.com>
2024-02-12 10:44:15 +0530

    add

--- application/views/home/my_order/my_order.php
+++ application/views/home/my_order/my_order.php
@@ -32,15 +32,15 @@ function goBack() {
 			<div class="row">
 				<div class="col-sm-12 text-center">
 					<span class="load_page_loading" style="position: fixed;top: 300px;z-index: 100;margin-left:-90px"></span>
-				</div>51
+				</div>
 				<div class="col-sm-12" style="margin-top:10px;">
 					<button onclick="load_more()" class="load_more"></button>
 				</div>
-			</div>51
+			</div>
 		</div>
 	</div>     
 </div>
-<input type="hidden" class="get_record" value="0">51
+<input type="hidden" class="get_record" value="0">
 <script>
 $(window).scroll(function(){
 	var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();

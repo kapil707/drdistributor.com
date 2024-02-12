2f047c8b399853e18f8f399e0f0ce277eb667dce
kapil-asus-new <kapil707sharma@gmail.com>
2024-02-12 16:30:50 +0530

    add

--- application/controllers/My_order.php
+++ application/controllers/My_order.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class My_order extends CI_Controller {
 	
 	public function index(){
-		echo "sdafF";
 		////error_reporting(0);
 		//$this->login_check();
 		//$this->salesman_chemist_ck();

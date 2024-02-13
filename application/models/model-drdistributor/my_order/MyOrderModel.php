f6c30c436e66d6cf712ddd31fe81e0054d394dc8
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-13 09:52:27 +0530

    add

--- application/models/model-drdistributor/my_order/MyOrderModel.php
+++ application/models/model-drdistributor/my_order/MyOrderModel.php
@@ -55,9 +55,9 @@ class MyOrderModel extends CI_Model
 			);
 			$jsonArray[] = $dt;
 		}
-		$jsonString  = json_encode($jsonArray);
+		//$jsonString  = json_encode($jsonArray);
 		
-		$return["items"] 		= $jsonString;
+		$return["items"] 		= $jsonArray;
 		$return["get_record"] 	= $get_record;
 		return $return;
 	}

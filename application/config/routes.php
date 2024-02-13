b6d8e86d46c706c6029aa2f10ede6689d65d70a4
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-13 14:00:49 +0530

    add

--- application/config/routes.php
+++ application/config/routes.php
@@ -79,8 +79,8 @@ $route['medicine_search']='medicine_search/index';
 $route['home/search_medicine']='medicine_search/index';
 $route['home/medicine_search']='medicine_search/index';
 
-
 $route['my_cart']='my_cart/index';
+$route['home/my_cart']='my_cart/index';
 
 
 $route['track_order']='home/track_order';

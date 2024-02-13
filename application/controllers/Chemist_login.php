4093df5f9895a57abde1afcf4e4bf2063ada10bb
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-13 16:26:22 +0530

    add

--- application/controllers/Chemist_login.php
+++ application/controllers/Chemist_login.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-class User extends CI_Controller {
+class Chemist_login extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model("model-drdistributor/chemist_login/ChemistLoginModel");

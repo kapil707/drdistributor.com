d473f6a459baaa1f9346c161f6109fbc88c4852a
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-13 14:45:31 +0530

    add

--- application/views/home/search_medicine/search_medicine.php
+++ application/views/home/search_medicine/search_medicine.php
@@ -92,7 +92,7 @@ function goBack() {
 			</span>
 		</div>
 		<div class="col-sm-3 col-12 mobile_off" style="margin-bottom:5px;">
-			<h6 class="search_pg_title_color" onclick="current_order_ref()">Current order <span class="div_cart_total_items1"></span></h6>
+			<h6 class="search_pg_title_color" onclick="current_order_ref()">My Cart <span class="div_cart_total_items1"></span></h6>
 		</div>
 		<div class="col-sm-3 col-12 mobile_off text-right" style="margin-bottom:5px;">
 			<a href="#" onclick="delete_all_medicine()" tabindex="-10" class="cart_delete_btn delete_all_btn" title="Delete all medicines">
@@ -526,7 +526,7 @@ function medicine_cart_list()
 					$(".div_cart_total_items1").html("("+items_total+")");
 					$(".header_cart_span").html(items_total);
 					$(".place_order_message").html(place_order_message);
-					$(".header_result_found").html("Current order ("+items_total+")");
+					$(".header_result_found").html("My Cart ("+items_total+")");
 					if(items_total==0)
 					{
 						$(".cart_empty_cart_div").show();

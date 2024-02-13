a8091d46343745ddf54005fab4de83b8343739f8
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-13 14:47:23 +0530

    add

--- application/views/home/my_cart/my_cart.php
+++ application/views/home/my_cart/my_cart.php
@@ -253,7 +253,7 @@ function medicine_cart_list()
 					$(".medicine_cart_list_div").append('<div class="main_theme_li_bg"><div class="medicine_cart_div1">'+image_div+'</div><div class="medicine_cart_div2"><div class="medicine_cart_item_name" title="'+item_name+'">'+item_name+' <span class="medicine_cart_item_packing">('+item_packing+' Packing)</span></div><div class="medicine_cart_item_expiry">Expiry : '+item_expiry+'</div><div class="medicine_cart_item_company">By '+item_company+'</div><div class="text-left medicine_cart_item_order_quantity" title="'+item_name+' Quantity: '+item_order_quantity+'" >Order quantity : '+item_order_quantity+item_scheme_div+'</div><span class="mobile_off">'+rate_div+'</span></div><span class="mobile_show" style="margin-left:5px;">'+rate_div+'</span></div>'+div_all_data);
 				}
 			});
-			$.each(data.other_items, function(i,item){
+			$.each(data.items_other, function(i,item){
 				if (item)
 				{
 					items_price = item.items_price;

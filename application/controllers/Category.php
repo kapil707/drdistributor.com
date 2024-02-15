f4c2d1515de758fd4c1ffd54252fecae81953afd
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-15 13:24:18 +0530

    add

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -86,7 +86,7 @@ class Category extends CI_Controller {
 				$title  = $result["title"];
 				$get_record  = $result["get_record"];
 			}
-			
+
 			if($item_page_type=="featured_brand")
 			{
 				$result = $this->MedicineCategoryModel->featured_brand_api($session_yes_no,$item_code,$item_division,$get_record);
@@ -129,7 +129,7 @@ class Category extends CI_Controller {
 		$data["session_user_image"] 	= $_COOKIE['user_image'];
 		$data["session_user_fname"]     = $_COOKIE['user_fname'];
 		$data["session_user_altercode"] = $_COOKIE['user_altercode'];
-		$data["chemist_id"] 			= $_COOKIE['user_altercode'];
+		$data["chemist_id"] 			= "";
 		
 		$data["main_page_title"] = "Dr";
 		if(empty($_COOKIE['user_session'])){
@@ -145,10 +145,11 @@ class Category extends CI_Controller {
 		$user_altercode = $_COOKIE["user_altercode"];
 		$user_password	= $_COOKIE["user_password"];
 
-		$chemist_id 	= $_COOKIE["chemist_id"];
+		$chemist_id 	= "";
 		$salesman_id = "";
 		if($user_type=="sales")
 		{
+			$chemist_id 	= $_COOKIE["chemist_id"];
 			$salesman_id 	= $user_altercode;
 			$user_altercode = $chemist_id;
 		}
@@ -161,7 +162,7 @@ class Category extends CI_Controller {
 		$this->Chemist_Model->user_activity_log($user_type,$user_altercode,$salesman_id,$page_name,$browser_type,$browser);
 		/********************************************************** */
 
-		$this->load->view('home/header', $data);		
+		$this->load->view('home/header_footer/header', $data);		
 		$this->load->view('home/category/medicine_category', $data);
 	}
 	

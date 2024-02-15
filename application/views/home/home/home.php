535fa29c11dc030b7603ad03c7be4de4535525a4
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-15 09:42:14 +0530

    add

--- application/views/home/home/home.php
+++ application/views/home/home/home.php
@@ -226,11 +226,11 @@ function home_page_itemcategory(category_id,result_row,title){
 
 			item_other_image_div = '';
 			if(item_featured=="1" && item_quantity!="0"){
-				item_other_image_div = '<img src="<?= base_url() ?>img_v<?= $site_v ?>/featured_img.png" class="medicine_cart_item_featured_img">';
+				item_other_image_div = '<img src="<?= base_url() ?>img_v51/featured_img.png" class="medicine_cart_item_featured_img">';
 			}
 
 			if(item_quantity==0) {
-				item_other_image_div = '<img src="<?= base_url() ?>img_v<?= $site_v ?>/out_of_stock_img.png" class="medicine_cart_item_out_of_stock_img">';
+				item_other_image_div = '<img src="<?= base_url() ?>img_v51/out_of_stock_img.png" class="medicine_cart_item_out_of_stock_img">';
 			}
 
 			mydata+= '<div class="item"><div class="home_main_div"><div class="image"><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')">'+item_other_image_div+'<img src="'+item_image+'" alt="" '+error_img+' class="medicine_cart_item_image"></a></div><div class="content"><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')"><div class="medicine_cart_item_name">'+item_name+'<span class="medicine_cart_item_packing"> ('+item_packing+' Packing)</span></div><div class="medicine_cart_item_margin">'+item_margin+'% Margin*</div><div class="medicine_cart_item_company">By '+item_company+'</div><div class="medicine_cart_item_mrp">MRP : <i class="fa fa-inr" aria-hidden="true"></i> '+item_mrp+'/-</div><div class="medicine_cart_item_ptr">PTR : <i class="fa fa-inr" aria-hidden="true"></i> '+item_ptr+'/-</div><div class="medicine_cart_item_price">*Approximate ~ : <i class="fa fa-inr" aria-hidden="true"></i> '+item_price+'/-</div></a></div></div></div>';

0a1cfbc5dd44e95c00bcaf79c432e5f9cb5765df
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-16 21:00:40 +0530

    add

--- application/config/routes.php
+++ application/config/routes.php
@@ -48,7 +48,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'main';
+$route['default_controller'] = 'Home/index';
 
 $route['login'] = 'Chemist_login/login';
 $route['register'] = 'Chemist_login/logout';

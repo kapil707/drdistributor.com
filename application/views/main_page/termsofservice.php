8c25ee92a4948844b7210051d28415d13276a55f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-16 16:07:43 +0530

    add

--- application/views/main_page/termsofservice.php
+++ application/views/main_page/termsofservice.php
@@ -21,12 +21,11 @@ $(".headertitle").html("Terms of services");
 	<div class="row">
 		<div class="col-sm-12 col-12">
 			<div class="row">
-				<div class="col-sm-12 col-12">
-				
-				This site is owned and operated by  DR. Distributor Pvt. Ltd. here link is <a href="http://www.drdistributor.com/">http://www.drdistributor.com/</a>. Your privacy is of the utmost importance to us. At  DR. Distributor we want to make your online experience satisfying and safe.<br><br>
-				Because we gather certain types of information about our users, we feel you should fully understand our policy and the terms and conditions surrounding the capture and use of that information. This privacy statement discloses what information we gather and how we use it.<br><br>  
-				DR. DistributorProduct Outsourcing provides information on its website.  http://drdistributor.com/.   to the general public subject to the terms and conditions included in Section I of this Statement. By using the  DR. Distributor Product Outsourcing  Public Site, as defined below, you agree to be bound by the terms and conditions contained in Section I below.<br><br>
-			 	DR. Distributor Product Outsourcing offers enhanced, special Website access to registered Members, who are required to agree to the additional terms & conditions contained in Section II below. The Section II terms and conditions apply only to Members<br><br>
+				<div class="col-sm-12 col-12">				
+					<p>This site is owned and operated by  DR. Distributor Pvt. Ltd. here link is <a href="http://www.drdistributor.com/">http://www.drdistributor.com/</a>. Your privacy is of the utmost importance to us. At  DR. Distributor we want to make your online experience satisfying and safe.<p>
+					<p>Because we gather certain types of information about our users, we feel you should fully understand our policy and the terms and conditions surrounding the capture and use of that information. This privacy statement discloses what information we gather and how we use it.</p>
+					<p>D.R. Distributor Product Outsourcing provides information on its website. <a href="http://www.drdistributor.com/">http://www.drdistributor.com/</a>.   to the general public subject to the terms and conditions included in Section I of this Statement. By using the  DR. Distributor Product Outsourcing  Public Site, as defined below, you agree to be bound by the terms and conditions contained in Section I below.</p>
+					<p>D.R. Distributor Product Outsourcing offers enhanced, special Website access to registered Members, who are required to agree to the additional terms & conditions contained in Section II below. The Section II terms and conditions apply only to Members</p>
 				</div>
 			</div>
 		</div>

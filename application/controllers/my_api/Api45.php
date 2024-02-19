783bd7d67c462ba2dd7536a6dd5703fdfcc68556
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-19 16:21:43 +0530

    add

--- application/controllers/my_api/Api45.php
+++ application/controllers/my_api/Api45.php
@@ -26,4 +26,39 @@ class Api45 extends CI_Controller {
         header('Content-Type: application/json');
         echo "[".json_encode($response)."]";
 	}
+
+	public function my_notification_api(){
+		$this->load->model("model-drdistributor/my_notification/MyNotificationModel");
+		
+		$api_key		= $_POST['api_key'];
+		$user_type 		= $_POST["user_type"];
+		$user_altercode = $_POST["user_altercode"];
+		$user_password	= $_POST["user_password"];
+		$chemist_id 	= $_POST["chemist_id"];
+		$get_record	 	= $_POST["get_record"];
+		$salesman_id 	= "";
+		if($user_type=="sales")
+		{
+			$salesman_id 	= $user_altercode;
+			$user_altercode = $chemist_id;
+		}
+		$items = $get_record = "";
+		if(!empty($user_type) && !empty($user_altercode)) {
+
+			$result = $this->MyNotificationModel->get_my_notification_api($user_type,$user_altercode,$salesman_id,$get_record);
+			$items  	= $result["items"];
+			$get_record  = $result["get_record"];
+		}
+
+		$response = array(
+            'success' => "1",
+            'message' => 'Data load successfully',
+            'items' => $items,
+            'get_record' => $get_record
+        );
+
+        // Send JSON response
+        header('Content-Type: application/json');
+        echo "[".json_encode($response)."]";
+	}
 }

e99382e9f2ee0e21676c2351c120fcc26d811c18
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-21 13:24:17 +0530

    add

--- application/controllers/my_api/Api45.php
+++ application/controllers/my_api/Api45.php
@@ -347,4 +347,37 @@ class Api45 extends CI_Controller {
         header('Content-Type: application/json');
         echo "[".json_encode($response)."]";
 	}
+
+	public function medicine_details_api()
+	{
+		$this->load->model("model-drdistributor/medicine_details/MedicineDetailsModel");
+		
+		$item_code		= $_REQUEST["item_code"];
+		$user_type 		= $_COOKIE["user_type"];
+		$user_altercode = $_COOKIE["user_altercode"];
+		$user_password	= $_COOKIE["user_password"];
+		$chemist_id 	= "";
+		$salesman_id = "";
+		if($user_type=="sales")
+		{
+			$chemist_id 	= $_COOKIE["chemist_id"];
+			$salesman_id 	= $user_altercode;
+			$user_altercode = $chemist_id;
+		}
+		if(!empty($user_type) && !empty($user_altercode) && !empty($item_code)){
+			
+			$result = $this->MedicineDetailsModel->medicine_details_api($user_type,$user_altercode,$salesman_id,$item_code);
+			$items = $result["items"];
+		}
+        
+        $response = array(
+            'success' => "1",
+            'message' => 'Data load successfully',
+            'items' => $items
+        );
+
+        // Send JSON response
+        header('Content-Type: application/json');
+        echo "[".json_encode($response)."]";
+	}
 }

6619200dc93835ae7de27ab7c8f5a12f88ddbe7c
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-02-23 16:27:40 +0530

    add

--- application/views/home/user/update_account.php
+++ application/views/home/user/update_account.php
@@ -35,15 +35,24 @@ function goBack() {
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-12 mt-2">
+				<div class="col-sm-12">
+					<h5 class="mt-2">Enter mobile number</h5>
+				</div>
+				<div class="col-sm-12">
 					<img src="<?= base_url() ?>/img_v51/phone1.png" width="25px" style="float: left; margin-top: 7px;position: absolute;margin-left: 10px;" alt>
 					<input type="text" value="" class="input_type_text login_textbox" placeholder="Enter mobile number" required="" name="mobile1" id="mobile1" title="Enter mobile number" style="padding-left:40px;float: left;">
 				</div>
-				<div class="col-sm-12 mt-2">
+				<div class="col-sm-12">
+					<h5 class="mt-2">Enter email address</h5>
+				</div>
+				<div class="col-sm-12">
 					<img src="<?= base_url() ?>/img_v51/email1.png" width="25px" style="float: left; margin-top: 7px;position: absolute;margin-left: 10px;" alt>
 					<input type="text" value="" class="input_type_text login_textbox" placeholder="Enter email address" required="" name="email1" id="email1" title="Enter email" style="padding-left:40px;float: left;">
 				</div>
-				<div class="col-sm-12 mt-2">
+				<div class="col-sm-12">
+					<h5 class="mt-2">Enter address</h5>
+				</div>
+				<div class="col-sm-12">
 					<img src="<?= base_url() ?>/img_v51/map1.png" width="25px" style="float: left; margin-top: 7px;position: absolute;margin-left: 10px;" alt>
 					<input type="text" value="" class="input_type_text login_textbox" placeholder="Enter address" required="" name="address1" id="address1" title="Enter address" style="padding-left:40px;float: left;">
 				</div>

d20450a7c3f78b78fe0cd070a2c01136e908456f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-02 10:39:35 +0530

    add

--- application/views/home/home/home.php
+++ application/views/home/home/home.php
@@ -281,7 +281,12 @@ function home_page_itemcategory(category_id,items,title){
 				item_other_image_div = '<img src="<?= base_url() ?>img_v51/out_of_stock_img.png" class="medicine_cart_item_out_of_stock_img">';
 			}
 
-			mydata+= '<div class="item"><div class="featured_brand_home_div text-center"><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')">'+item_other_image_div+'<img src="'+item_image+'" alt="" '+error_img+' class="medicine_cart_item_image"></a><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')"><div class="medicine_cart_item_name">'+item_name+'<span class="medicine_cart_item_packing"> ('+item_packing+' Packing)</span></div><div class="medicine_cart_item_margin">'+item_margin+'% Margin*</div><div class="medicine_cart_item_company">By '+item_company+'</div><div class="medicine_cart_item_mrp">MRP : <i class="fa fa-inr" aria-hidden="true"></i> '+item_mrp+'/-</div><div class="medicine_cart_item_ptr">PTR : <i class="fa fa-inr" aria-hidden="true"></i> '+item_ptr+'/-</div><div class="medicine_cart_item_price">*Approximate ~ : <i class="fa fa-inr" aria-hidden="true"></i> '+item_price+'/-</div></a></div></div>';
+			item_scheme_div = "";
+			if(item_scheme!="0+0") {
+				item_scheme_div = '<div class="medicine_cart_item_scheme">Scheme : '+item_scheme+'</div>';
+			}
+
+			mydata+= '<div class="item"><div class="featured_brand_home_div text-center"><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')">'+item_other_image_div+'<img src="'+item_image+'" alt="" '+error_img+' class="medicine_cart_item_image"></a><a href="javascript:void(0)" onClick="medicine_details_funcation('+item_code+')"><div class="medicine_cart_item_name">'+item_name+'<span class="medicine_cart_item_packing"> ('+item_packing+' Packing)</span></div><div class="medicine_cart_item_margin">'+item_margin+'% Margin*</div><div class="medicine_cart_item_company">By '+item_company+'</div>'+item_scheme_div+'<div class="medicine_cart_item_mrp">MRP : <i class="fa fa-inr" aria-hidden="true"></i> '+item_mrp+'/-</div><div class="medicine_cart_item_ptr">PTR : <i class="fa fa-inr" aria-hidden="true"></i> '+item_ptr+'/-</div><div class="medicine_cart_item_price">*Approximate ~ : <i class="fa fa-inr" aria-hidden="true"></i> '+item_price+'/-</div></a></div></div>';
 		}
 	});
 	

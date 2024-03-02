46e9b5e142bc88c692f67422a1c116a2e43e9327
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-02 11:34:58 +0530

    add

--- application/models/model-drdistributor/my_notification/MyNotificationModel.php
+++ application/models/model-drdistributor/my_notification/MyNotificationModel.php
@@ -111,7 +111,7 @@ class MyNotificationModel extends CI_Model
 			
 			$item_image = $user_image;
 
-			$item_message = nl2br($item_message);
+			$item_message = str_replace("\\n","<br>",$item_message);
 			
 			$dt = array(
 				'item_id' => $item_id,

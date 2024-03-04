fd08f944680900c1afb69c1d732bafdd9dbcd5c1
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-04 13:48:40 +0530

    add

--- application/views/my_order/my_order_details.php
+++ application/views/my_order/my_order_details.php
@@ -24,7 +24,7 @@ function goBack() {
 	<div class="row">
 		<div class="col-sm-12 col-12">
 			<div class="row">
-				<div class="col-sm-12 col-12">
+				<div class="col-sm-12 col-12 p-2">
 					<div class="website_box_part load_page" style="display:none">
 					</div>
 				</div>

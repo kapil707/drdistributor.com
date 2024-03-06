78b4485cb721396112fd38bb15e57ac43c78a133
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-06 13:39:29 +0530

    add

--- application/models/model-drdistributor/account_model/AccountModel.php
+++ application/models/model-drdistributor/account_model/AccountModel.php
@@ -277,7 +277,7 @@ class AccountModel extends CI_Model
 						{
 							$date = date('Y-m-d');
 							$delete_request_date = date('Y-m-d', strtotime($date. ' + 6 day'));
-							$this->db->query("update tbl_acm_other set block=1,delete_request=1,delete_request_date='$delete_request_date' where code='$query->code'");
+							$this->db->query("update tbl_acm_other set block=1,status=0,delete_request=1,delete_request_date='$delete_request_date' where code='$query->code'");
 
 							$group2_message = "Hello Team Account Delete Request<br><br>Chemist Code : ".$user_name."<br>Mobile Number : ".$phone_number."<br><br>Thanks";
 							/***************only for group message***********************/
@@ -290,7 +290,7 @@ class AccountModel extends CI_Model
 						}
 						else
 						{
-							$status_message = "Your account deleted.";
+							$status_message = "User account is blocked.";
 						}
 					}
 					else

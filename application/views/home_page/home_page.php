9ad66075e195afc90fe3069c4589f6b52c0b193f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-15 17:50:49 +0530

    add

--- application/views/home_page/home_page.php
+++ application/views/home_page/home_page.php
@@ -468,6 +468,9 @@ $(document).ready(function() {
 	home_page_api(5);
 	
     $(window).scroll(function(){
+		console.log("scrollTop"+$(window).scrollTop())
+		console.log("document-height"+$(window).scrollTop())
+		console.log("window-height"+$(window).scrollTop())
 		if(($(window).scrollTop() == $(document).height() - $(window).height()) && query_work==0){
 			home_page_api(next_id);
 		}

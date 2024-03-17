8b0d5cd69abaf506351c5aadf74b39b6cde63e62
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-17 18:09:06 +0530

    add

--- application/views/header_footer/header.php
+++ application/views/header_footer/header.php
@@ -1167,7 +1167,7 @@ function change_item_order_quantity(){
 
 			item_price_calculate = item_price_calculate.toFixed(2);
 
-			$(".medicine_details_item_price_calculate").html('Total : <i class="fa fa-inr" aria-hidden="true"></i> ' +item_price_calculate + "/-")
+			$(".medicine_details_item_price_calculate").html('Total : <i class="fa fa-inr" aria-hidden="true"></i> ' +item_price_calculate + "/-");
 		}else{
 			$(".add_to_cart_error_message").html('Enter maximum quantity '+item_quantity+' only');
 			$(".medicine_details_item_add_to_cart_btn").hide();

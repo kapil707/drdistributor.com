f2598823b4d30b14b9c254df8027216efa766282
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-19 17:40:02 +0530

    add

--- application/views/my_invoice/my_invoice.php
+++ application/views/my_invoice/my_invoice.php
@@ -55,18 +55,18 @@ function call_page(get_record)
 		$.ajax({
 			type       : "POST",
 			dataType   : "json",
-			data       :  { get_record:get_record} ,
+			data       : {get_record:get_record} ,
 			url        : "<?php echo base_url(); ?>my_invoice/my_invoice_api",
 			cache	   : false,
 			error: function(){
 				$(".main_page_loading").html("");
-				$(".load_page").html('<h2><img src="<?= base_url(); ?>img_v51/something_went_wrong.png" width="100%"></h2>');
+				$(".main_page_data").html('<h2><img src="<?= base_url(); ?>img_v51/something_went_wrong.png" width="100%"></h2>');
 			},
 			success    : function(data){
 
-				$(".load_page_loading").html("");				
+				$(".main_page_loading").html("");				
 				if(data.items=="" && no_record_found=="0") {
-					$(".main_page_loading").html('<h2><center><img src="<?= base_url(); ?>/img_v51/no_record_found.png" width="100%"></center></h2>');
+					$(".main_page_data").html('<h2><center><img src="<?= base_url(); ?>/img_v51/no_record_found.png" width="100%"></center></h2>');
 				}
 				
 				get_record 	= data.get_record;
@@ -96,7 +96,7 @@ function call_page(get_record)
 							delete_status_div = '<div class="all_item_date_time">Some items have been deleted / modified in this order</div>';
 						}
 						
-						$(".main_page_data").append('<div class="main_box_div_data"><div class="all_page_box_left_div"><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><img src="'+item_image+'" alt="" title="" onerror=this.src="<?= base_url(); ?>/uploads/default_img.jpg" class="all_item_image"></a></div><div class="all_page_box_right_div text-left"><div class=""><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><span class="all_item_name">'+item_title+'</span></a><span style="float: right;color: red;"><a href="'+download_url+'" style="color: red;">Download Invoice</a></span></div><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><div class="all_item_price">Total : <i class="fa fa-inr" aria-hidden="true"></i> '+item_total+'/-</div><div class="all_item_datetime">Invoice Date : '+item_date_time+''+out_for_delivery+'</div>'+delete_status_div+'</div></a></div>');
+						$(".main_page_data").append('<div class="main_box_div_data"><div class="all_page_box_left_div"><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><img src="'+item_image+'" alt="" title="" onerror=this.src="<?= base_url(); ?>/uploads/default_img.jpg" class="all_item_image"></a></div><div class="all_page_box_right_div text-left"><div class=""><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><span class="all_item_name">'+item_title+'</span></a><span style="float: right;color: red;"><a href="'+download_url+'"  class="all_item_download">Download Invoice</a></span></div><a href="<?php echo base_url() ?>my_invoice_details/'+item_id+'"><div class="all_item_price">Total : <i class="fa fa-inr" aria-hidden="true"></i> '+item_total+'/-</div><div class="all_item_date_time">Invoice Date : '+item_date_time+''+out_for_delivery+'</div>'+delete_status_div+'</div></a></div>');
 						
 						query_work = 0;
 						no_record_found = 1;

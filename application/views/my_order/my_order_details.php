2c1fe337465dccddf6d1bf2326e02b7f2c44be2b
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-19 09:57:51 +0530

    add

--- application/views/my_order/my_order_details.php
+++ application/views/my_order/my_order_details.php
@@ -24,4 +24,4 @@ function goBack() {
 <script>
 	var item_id = "<?php echo $item_id; ?>";
 </script>
-<script src="<?php echo base_url(); ?>/assets/js/my_order_details123eifghi.js"></script>
+<script src="<?php echo base_url(); ?>/assets/js/my_order_details123eifghij.js"></script>

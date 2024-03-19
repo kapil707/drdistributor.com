9f4967973cff79c364c5b4994a38c2737927b85c
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-19 16:10:21 +0530

    add

--- application/controllers/Chemist_select.php
+++ application/controllers/Chemist_select.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Chemist_search extends CI_Controller {
+class chemist_select extends CI_Controller {
 	
 	public function __construct(){
 		parent::__construct();

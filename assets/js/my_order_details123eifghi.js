ca1d8243fd09424174a2dec9941fda3bd91c7fb1
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-19 09:57:26 +0530

    add

--- assets/js/my_order_details123eifghi.js
+++ assets/js/my_order_details123eifghi.js
@@ -45,6 +45,7 @@ function call_page()
 					item_ptr			= "";
 					item_mrp			= "";
 					item_featured		= "";
+					item_margin			= "";
 
 					div_all_data = "<span class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></span>";
 					

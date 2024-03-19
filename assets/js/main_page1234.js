c301432d5a34cda740a191cce7b7000c4a987996
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-19 15:22:31 +0530

    add

--- assets/js/main_page1234.js
+++ assets/js/main_page1234.js
@@ -471,7 +471,7 @@ function medicine_add_to_cart_api()
 								status = item.status;
 								status_message = item.status_message;
 								$(".search_cart_footer_div_total_price").html('<i class="fa fa-inr"></i>'+items_price+'/-');
-								$(".search_cart_footer_div_total_items").html("My Cart ("+items_total+")");
+								$(".search_cart_footer_div_total_items").html("My cart ("+items_total+")");
 								$(".search_cart_page_total_cart_items").html("("+items_total+")");
 								$(".header_cart_span").html(items_total);
 								$(".place_order_message").html(status_message);
@@ -695,7 +695,7 @@ function my_cart_api()
 					status = item.status;
 					status_message = item.status_message;
 					$(".search_cart_footer_div_total_price").html('<i class="fa fa-inr"></i>'+items_price+'/-');
-					$(".search_cart_footer_div_total_items").html("My Cart ("+items_total+")");
+					$(".search_cart_footer_div_total_items").html("My cart ("+items_total+")");
 					$(".search_cart_page_total_cart_items").html("("+items_total+")");
 					$(".header_cart_span").html(items_total);
 					$(".place_order_message").html(status_message);

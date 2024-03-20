b7393d6b493c2240e73e316d487d90bd31e4962f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-20 12:33:28 +0530

    add

--- application/models/model-drdistributor/my_notification/MyNotificationModel.php
+++ application/models/model-drdistributor/my_notification/MyNotificationModel.php
@@ -111,6 +111,7 @@ class MyNotificationModel extends CI_Model
 			
 			$item_image = $user_image;
 
+			$item_message = str_replace("\\n", "<br>", $item_message);
 			$item_message = $this->new_clean($item_message);
 			$item_message = str_replace("br", "<br>", $item_message);
 			

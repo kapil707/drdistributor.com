3e70f2da99b1d8ae7103152ed61d34ec60d517ac
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 11:05:19 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -2,7 +2,19 @@
 <link rel="stylesheet" href="<?= base_url(); ?>assets/website/easyzoom/easyzoom.css" />
 
 
-<div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails">
+
+<div class="thumbnails">
+    <img src="thumbnail1.jpg" data-full-image="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png" class="thumbnail" />
+    <img src="thumbnail2.jpg" data-full-image="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg" class="thumbnail" />
+    <!-- Add more thumbnails as needed -->
+</div>
+
+<div class="zoom-container">
+    <img src="" alt="" class="zoom-image" />
+</div>
+
+
+<!-- <div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails">
     <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png">
         <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png" alt="" width="640" height="360" />
     </a>
@@ -29,12 +41,20 @@
             <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860695photocil-Photoroom_(1).jpg" alt="" />
         </a>
     </li>
-</ul>
+</ul> -->
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
 <script>
-var $easyzoom = $('.easyzoom').easyZoom();
+
+$(document).ready(function() {
+    $('.thumbnail').on('click', function() {
+        var fullImage = $(this).data('full-image');
+        $('.zoom-image').attr('src', fullImage);
+        $('.zoom-image').easyZoom();
+    });
+});
+// var $easyzoom = $('.easyzoom').easyZoom();
 
 // $(document).ready(function() {
 //     setTimeout(function() {

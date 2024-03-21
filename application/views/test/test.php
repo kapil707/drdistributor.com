cd45058c3d2e175a25e2cad5bd40ab3c290b1278
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 09:43:13 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -2,7 +2,7 @@
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
 
 
-<div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails">
+<div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails" style="width:300px;height:300px;">
 	<a class="example-image-link" data-standard="">
 		<img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" width="100%" style="float: right;margin-top:10px;" class="medicine_details_image modal_item_image_change" alt="zoom" loading="eager" onerror="setDefaultImage(this);">
 	</a>

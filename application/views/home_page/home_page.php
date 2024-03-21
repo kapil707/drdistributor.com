2cbc6b892caa9403fa3dcaf2acfbafc7764c7640
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 14:31:16 +0530

    add

--- application/views/home_page/home_page.php
+++ application/views/home_page/home_page.php
@@ -25,92 +25,7 @@ $default_img = base_url()."/uploads/default_img.jpg";
 $error_img ="onerror=this.src=".base_url()."/uploads/default_img.jpg";
 
 ?>
-<script src="<?= base_url(); ?>assets/js/jssor.slider-28.0.0.min.js" type="text/javascript"></script>
-<script>
-window.jssor_1_slider_init = function() {
-
-	var jssor_1_options = {
-		$AutoPlay: 1,
-		$SlideWidth: 700,
-		$ArrowNavigatorOptions: {
-		$Class: $JssorArrowNavigator$
-		},
-		$BulletNavigatorOptions: {
-		$Class: $JssorBulletNavigator$
-		}
-	};
-
-	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-	/*#region responsive code begin*/
-
-	var MAX_WIDTH = screen.width;
-
-	function ScaleSlider() {
-		var containerElement = jssor_1_slider.$Elmt.parentNode;
-		var containerWidth = containerElement.clientWidth;
-
-		if (containerWidth) {
-
-			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-			jssor_1_slider.$ScaleWidth(expectedWidth);
-		}
-		else {
-			window.setTimeout(ScaleSlider, 30);
-		}
-	}
-
-	ScaleSlider();
-
-	$Jssor$.$AddEvent(window, "load", ScaleSlider);
-	$Jssor$.$AddEvent(window, "resize", ScaleSlider);
-	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-	/*#endregion responsive code end*/
-};
-
-window.jssor_2_slider_init = function() {
-
-	var jssor_2_options = {
-		$AutoPlay: 1,
-		$SlideWidth: 700,
-		$ArrowNavigatorOptions: {
-		$Class: $JssorArrowNavigator$
-		},
-		$BulletNavigatorOptions: {
-		$Class: $JssorBulletNavigator$
-		}
-	};
-
-	var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_2_options);
-
-	/*#region responsive code begin*/
-
-	var MAX_WIDTH = screen.width;
-
-	function ScaleSlider2() {
-		var containerElement = jssor_2_slider.$Elmt.parentNode;
-		var containerWidth = containerElement.clientWidth;
-
-		if (containerWidth) {
-
-			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-			jssor_2_slider.$ScaleWidth(expectedWidth);
-		}
-		else {
-			window.setTimeout(ScaleSlider2, 30);
-		}
-	}
-
-	ScaleSlider2();
-
-	$Jssor$.$AddEvent(window, "load", ScaleSlider2);
-	$Jssor$.$AddEvent(window, "resize", ScaleSlider2);
-	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider2);
-	/*#endregion responsive code end*/
-};
-</script>
+<script src="<?= base_url(); ?>assets/website/js/jssor.slider-28.0.0.min.js" type="text/javascript"></script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
@@ -122,4 +37,4 @@ window.jssor_2_slider_init = function() {
 	<div class="row home_page_invoice_notification_data"></div>
 	<div class="row home_page_all_data"></div>
 </div>
-<script src="<?php echo base_url(); ?>/assets/js/home_page.js"></script>
+<script src="<?php echo base_url(); ?>/assets/js/home_page1101.js"></script>
--- assets/js/home_page1101.js
+++ assets/js/home_page1101.js
@@ -331,3 +331,88 @@ function download_invoice(url){
 	window.open(url, '_blank');
 	window.close();
 }
+
+/************************************ */
+window.jssor_1_slider_init = function() {
+
+	var jssor_1_options = {
+		$AutoPlay: 1,
+		$SlideWidth: 700,
+		$ArrowNavigatorOptions: {
+		$Class: $JssorArrowNavigator$
+		},
+		$BulletNavigatorOptions: {
+		$Class: $JssorBulletNavigator$
+		}
+	};
+
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+	/*#region responsive code begin*/
+
+	var MAX_WIDTH = screen.width;
+
+	function ScaleSlider() {
+		var containerElement = jssor_1_slider.$Elmt.parentNode;
+		var containerWidth = containerElement.clientWidth;
+
+		if (containerWidth) {
+
+			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+			jssor_1_slider.$ScaleWidth(expectedWidth);
+		}
+		else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+
+	ScaleSlider();
+
+	$Jssor$.$AddEvent(window, "load", ScaleSlider);
+	$Jssor$.$AddEvent(window, "resize", ScaleSlider);
+	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+	/*#endregion responsive code end*/
+};
+
+window.jssor_2_slider_init = function() {
+
+	var jssor_2_options = {
+		$AutoPlay: 1,
+		$SlideWidth: 700,
+		$ArrowNavigatorOptions: {
+		$Class: $JssorArrowNavigator$
+		},
+		$BulletNavigatorOptions: {
+		$Class: $JssorBulletNavigator$
+		}
+	};
+
+	var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_2_options);
+
+	/*#region responsive code begin*/
+
+	var MAX_WIDTH = screen.width;
+
+	function ScaleSlider2() {
+		var containerElement = jssor_2_slider.$Elmt.parentNode;
+		var containerWidth = containerElement.clientWidth;
+
+		if (containerWidth) {
+
+			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+			jssor_2_slider.$ScaleWidth(expectedWidth);
+		}
+		else {
+			window.setTimeout(ScaleSlider2, 30);
+		}
+	}
+
+	ScaleSlider2();
+
+	$Jssor$.$AddEvent(window, "load", ScaleSlider2);
+	$Jssor$.$AddEvent(window, "resize", ScaleSlider2);
+	$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider2);
+	/*#endregion responsive code end*/
+};

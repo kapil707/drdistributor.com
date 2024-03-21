85e8ba010aa45067c300e23b1123847ab7ea8730
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 10:12:43 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -2,12 +2,13 @@
 <link rel="stylesheet" href="<?= base_url(); ?>assets/website/easyzoom/easyzoom.css" />
 
 
+<img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" data-zoom-src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" class="easyzoom" />
 
-<div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails" style="width:300px;height:300px;">
+<!-- <div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails" style="width:300px;height:300px;">
     <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" class="myacss">
         <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" alt="" class="myimgcss" style="width:300px;height:300px;" />
     </a>
-</div>
+</div> -->
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
@@ -18,7 +19,7 @@ $(document).ready(function() {
     setTimeout(function() {
 
 		// Update the image element with the loaded image URL
-		$('.myimgcss').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
+		$('.easyzoom').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
 
 		// Initialize EasyZoom on the updated image element
 		$('.easyzoom').easyZoom();

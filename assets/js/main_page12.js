6ec680ad70ba10385944a5d6457f4b2f924e303f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 15:37:57 +0530

    add

--- assets/js/main_page12.js
+++ assets/js/main_page12.js
@@ -321,9 +321,7 @@ function change_item_order_quantity(){
 }
 
 function medicine_add_to_cart_api()
-{
-	$(".medicine_search_textbox").focus();
-	
+{	
 	item_quantity		= $(".medicine_details_item_order_quantity_hidden").val();
 	item_order_quantity	= $(".medicine_details_item_order_quantity_textbox").val();
 	item_code			= $(".medicine_details_item_code").val();

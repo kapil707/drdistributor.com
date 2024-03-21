c8759a198a08238d0e5f1164a96b7477bad5d1e4
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 10:11:24 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -18,7 +18,7 @@ $(document).ready(function() {
     setTimeout(function() {
 
 		// Update the image element with the loaded image URL
-		$('.easyzoom').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
+		$('.myimgcss').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
 
 		// Initialize EasyZoom on the updated image element
 		$('.easyzoom').easyZoom();

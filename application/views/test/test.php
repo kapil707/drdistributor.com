af60bf3e6da47a26f8ddf4d4c0dd7fad5dced7d9
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 10:03:01 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -5,7 +5,7 @@
 
 <div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails" style="width:300px;height:300px;">
     <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg">
-        <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" alt="" style="width:300px;height:300px;" />
+        <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" alt="" class="myimgcss" style="width:300px;height:300px;" />
     </a>
 </div>
 
@@ -13,5 +13,14 @@
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
 <script>
 var $easyzoom = $('.easyzoom').easyZoom();
+
+$(document).ready(function() {
+    setTimeout(function() {
+        // Your code to load data goes here
+		$(".myimgcss").attr("src","https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png")
+        console.log('Data loaded after 5 seconds');
+		var $easyzoom = $('.easyzoom').easyZoom();
+    }, 5000); // 2000 milliseconds = 2 seconds
+});
 </script>
 

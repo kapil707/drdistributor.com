74763f918ef8a8d6ba9ed6bc2cbc934fdd2b1285
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 12:55:41 +0530

    add

--- application/views/home_page/home_page.php
+++ application/views/home_page/home_page.php
@@ -30,8 +30,6 @@
       position: absolute;
       top: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
     }
 
     /* Style for the actual image */
@@ -40,8 +38,6 @@
       position: absolute;
       top: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
     }
   </style>
 <?php

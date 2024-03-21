b5f7195d744276a84a4e93d7da8c737a7bd7493e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 11:21:06 +0530

    add

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -1,70 +1,24 @@
 
 <link rel="stylesheet" href="<?= base_url(); ?>assets/website/easyzoom/easyzoom.css" />
 
-
-
-<div class="thumbnails">
-    <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png" data-full-image="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png" class="thumbnail" />
-    <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg" data-full-image="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg" class="thumbnail" />
-    <!-- Add more thumbnails as needed -->
+<div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails" style="width:300px;height:300px;">
+    <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859763collagen_coffe-Photoroom_(1).jpg" alt="" class="myimgcss" style="width:300px;height:300px;" />
 </div>
-
-<div class="zoom-container">
-    <img src="" alt="" class="zoom-image" />
-</div>
-
-
-<!-- <div class="easyzoom easyzoom--overlay easyzoom--with-thumbnails">
-    <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png">
-        <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png" alt="" width="640" height="360" />
-    </a>
-</div>
-
-<ul class="thumbnails">
-    <li>
-        <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg" data-standard="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg">
-            <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860474corium_body-Photoroom_(1).jpg" alt="" />
-        </a>
-    </li>
-    <li>
-        <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859521marine-Photoroom_(1).jpg" data-standard="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859521marine-Photoroom_(1).jpg">
-            <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708859521marine-Photoroom_(1).jpg" alt="" />
-        </a>
-    </li>
-    <li>
-        <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708861436silky_af-Photoroom_(2).jpg" data-standard="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708861436silky_af-Photoroom_(2).jpg">
-            <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708861436silky_af-Photoroom_(2).jpg" alt="" />
-        </a>
-    </li>
-    <li>
-        <a href="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860695photocil-Photoroom_(1).jpg" data-standard="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860695photocil-Photoroom_(1).jpg">
-            <img src="https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708860695photocil-Photoroom_(1).jpg" alt="" />
-        </a>
-    </li>
-</ul> -->
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
 <script>
+var $easyzoom = $('.easyzoom').easyZoom();
 
 $(document).ready(function() {
-    $('.thumbnail').on('click', function() {
-        var fullImage = $(this).data('full-image');
-        $('.zoom-image').attr('src', fullImage);
-        $('.zoom-image').easyZoom();
-    });
+    setTimeout(function() {
+
+		// Update the image element with the loaded image URL
+		$('.easyzoom').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
+
+		// Initialize EasyZoom on the updated image element
+		$('.easyzoom').easyZoom();
+    }, 5000); // 2000 milliseconds = 2 seconds
 });
-// var $easyzoom = $('.easyzoom').easyZoom();
-
-// $(document).ready(function() {
-//     setTimeout(function() {
-
-// 		// Update the image element with the loaded image URL
-// 		$('.easyzoom').attr('src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png').attr('data-zoom-src', 'https://www.drdweb.co.in/uploads/manage_medicine_image/photo/resize/1708856059Cosmofix-R-Glow-Serum.png');
-
-// 		// Initialize EasyZoom on the updated image element
-// 		$('.easyzoom').easyZoom();
-//     }, 5000); // 2000 milliseconds = 2 seconds
-// });
 </script>
 

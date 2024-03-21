7ec7f5fecd452ab64554c6494e4dc423eb5633fd
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-21 14:07:59 +0530

    add

--- assets/js/medicine_category123456789t.js
+++ assets/js/medicine_category123456789t.js
@@ -1,7 +1,6 @@
 $(window).scroll(function(){
 	var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
 	if (scrollBottom<100){
-		//alert(parseInt($(window).scrollTop()))
 		load_more()
 	}
 });
@@ -19,40 +18,31 @@ var no_record_found = 0;
 var new_i = 0;
 function call_page(get_record)
 {
-	$(".top_bar_title2").html("Loading....");
 	if(query_work=="0")
 	{
-		console.log("working");
 		query_work = 1;
-		$(".main_page_loading").html('<div><center><img src="'+get_base_url()+'/img_v51/loading.gif" width="100px"></center></div><div><center>Loading....</center></div>');
+		$(".top_bar_title2").html("Loading....");
+		$(".main_page_loading").show();
 		$.ajax({
 			type       : "POST",
-			data       :  {item_page_type:item_page_type,item_code:item_code,item_division:item_division,get_record:get_record} ,
+			data       : {item_page_type:item_page_type,item_code:item_code,item_division:item_division,get_record:get_record} ,
 			url        : get_base_url()+"category/api/medicine_category_api",
 			cache	   : false,
 			error: function(){
-				$(".main_page_loading").html("");
-				$(".main_page_data").html('<div><img src="'+get_base_url()+'img_v51/something_went_wrong.png" width="100%"></div>');
 				$(".top_bar_title2").html("No record found");
+				$(".main_page_loading").hide();
+				$(".main_page_data").html('<h2><img src="'+get_base_url()+'img_v51/something_went_wrong.png" width="100%"></h2>');
 			},
 			success    : function(data){
+
+				$(".main_page_loading").hide();
 				if(data.items=="")
 				{
 					if(no_record_found=="0")
 					{
-						$(".main_page_loading").html("");
-						$(".main_page_data").html('<div><center><img src="'+get_base_url()+'/img_v51/no_record_found.png" width="100%"></center></div>');
 						$(".top_bar_title2").html("No record found");
+						$(".main_page_data").html('<h2><center><img src="'+get_base_url()+'/img_v51/no_record_found.png" width="100%"></center></h2>');
 					}
-					else
-					{
-						$(".load_page_loading").html("");
-						//$(".load_page").html("");
-					}
-				}
-				else
-				{
-					$(".load_page_loading").html("");
 				}
 				title 	= data.title;
 				if(title!=""){

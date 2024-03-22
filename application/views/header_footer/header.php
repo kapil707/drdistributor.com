e201a6619ee4c592f2d3a7504de599f4597ba760
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-22 17:02:12 +0530

    add

--- application/views/header_footer/header.php
+++ application/views/header_footer/header.php
@@ -467,44 +467,41 @@ if (!isset($_COOKIE["user_cart_total"])) {
 							<div class="col-sm-8 col-8 text-right">
 								<span class="medicine_details_item_price" title="*Approximate ~"></span>
 							</div>
-							<div class="col-sm-12 col-12 medicine_details_hr">
-							</div>
-							<div class="col-sm-12 col-12 text-left medicine_details_item_description1">
+							<div class="col-sm-12 col-12 text-left medicine_details_hr medicine_details_item_description1">
 								*The information given on this page is based on historical data and estimates . Please refer to the final invoice for the exact value. E&OE.
 							</div>
+							
+							<div class="col-sm-12 col-12 medicine_details_hr">
+							</div>
+
+							<div class="col-sm-12 col-12 order_quantity_div">
+								<div class="row">
+									<div class="col-sm-4 col-4">
+										<span class="medicine_details_item_order_quantity">Order quantity
+										</span>
+									</div>
+
+									<div class="col-sm-8 col-8 text-right">
+										<span class="medicine_details_item_total"></span>
+									</div>
+
+									<div class="col-sm-4 col-4">
+										<input type="number" class="medicine_details_item_order_quantity_textbox" placeholder="Eg 1,2" name="quantity" required="" style="width:100px;" value="" title="Enter quantity" min="1" max="1000" maxlength="4" onchange="change_item_order_quantity()" onkeyup="change_item_order_quantity()">
+										<input type="hidden" class="medicine_details_item_order_quantity_hidden">
+									</div>
+
+									<div class="col-sm-8 col-8">
+										<button type="submit" class="btn btn-primary main_theme_button medicine_details_item_add_to_cart_btn"  onclick="medicine_add_to_cart_api()" title="Add to cart">Add to cart</button>
+
+										<button type="submit" class="btn btn-primary main_theme_button_disable medicine_details_item_add_to_cart_btn_disable" onclick="" title="Add to cart">Add to cart</button>
+									</div>
+
+									<div class="col-sm-12 col-12 add_to_cart_error_message text-danger text-center medicine_details_hr"></div>
+								</div>
+							</div>
 						</div>
+
 					</div>
-					<div class="col-sm-12 col-12 medicine_details_hr">
-					</div>
-
-					<div class="col-sm-2 col-12"></div>
-					<div class="col-sm-8 col-12 order_quantity_div">
-						<div class="row">
-							<div class="col-sm-4 col-4">
-								<span class="medicine_details_item_order_quantity">Order quantity
-								</span>
-							</div>
-
-							<div class="col-sm-8 col-8 text-right">
-								<span class="medicine_details_item_total"></span>
-							</div>
-
-							<div class="col-sm-4 col-4">
-								<input type="number" class="medicine_details_item_order_quantity_textbox" placeholder="Eg 1,2" name="quantity" required="" style="width:100px;" value="" title="Enter quantity" min="1" max="1000" maxlength="4" onchange="change_item_order_quantity()" onkeyup="change_item_order_quantity()">
-								<input type="hidden" class="medicine_details_item_order_quantity_hidden">
-							</div>
-
-							<div class="col-sm-8 col-8">
-								<button type="submit" class="btn btn-primary main_theme_button medicine_details_item_add_to_cart_btn"  onclick="medicine_add_to_cart_api()" title="Add to cart">Add to cart</button>
-
-								<button type="submit" class="btn btn-primary main_theme_button_disable medicine_details_item_add_to_cart_btn_disable" onclick="" title="Add to cart">Add to cart</button>
-							</div>
-
-							<div class="col-sm-12 col-12 add_to_cart_error_message text-danger text-center medicine_details_hr"></div>
-						</div>
-					</div>
-					<div class="col-sm-2 col-12"></div>
-
 					<div class="col-sm-12 col-12 medicine_details_hr medicine_details_item_description2">
 					</div>
 				</div>

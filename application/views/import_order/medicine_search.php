facecaf10c98a922a7cbe43612a728e2c169cf4e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-22 19:02:07 +0530

    add

--- application/views/import_order/medicine_search.php
+++ application/views/import_order/medicine_search.php
@@ -152,7 +152,7 @@ function goBack() {
 				<button type="submit" class="btn btn-primary main_theme_button next_btn" name="submit" value="submit" style="width:20%">Next</button>
 			</a>
 		</div>
-		<div class="col-sm-12 col-12 col-padding-5 web-col-padding-5">
+		<div class="col-sm-12 col-12 col-padding-5 web-col-padding-5 mt-5">
 			<div class="main_box_div2 cart_page_div_for_fix_height">
 				<span class="my_cart_api_div"></span>
 			</div>
@@ -207,7 +207,6 @@ function change_order_quantity(row_id)
 						setTimeout(insert_main_row_data(row_id),500);
 					}
 					else{
-51
 						swal("Quantity not updated");
 					}
 				}
@@ -583,50 +582,6 @@ function add_new_row_import_order_page(item_code,item_order_quantity)
 	item_mrp = $(".medicine_details_all_data_"+item_code).attr("item_mrp")
 	$(".tbody_row").append('<tr class="remove_css_'+js_i+'"><td>'+js_j+'<input type="hidden" value="'+item_name+'" class="your_item_name_'+js_i+'"><input type="hidden" value="'+item_mrp+'" class="your_item_mrp_'+js_i+'"><input type="hidden" value="'+item_order_quantity+'" class="your_item_qty_'+js_i+'"></td><td class="import_order_td_'+js_i+'"><div class="row"><div class="col-sm-1"><img src="<?=base_url(); ?>img_v51/logo2.png" width="60px;" style="margin-top: 5px;" class="image_css_'+js_i+'" alt=""></div><div class="col-sm-11"><div class="row"><div class="col-sm-8"><span style="float:left;"><?= $myname;?> : <span class="import_order_title_1"> '+item_name+'</span> </span> <span style="float:left; margin-left:10px;margin-right:10px;" class="import_order_mrp"> | </span> <span style="float:left;"><a href="javascript:void(0)" onclick="delete_row_medicine('+js_i+')" title="Delete medicine" class="cart_delete_btn"> <img src="<?= base_url(); ?>img_v51/delete_icon.png" width="18px;" alt="Delete medicine"> Delete medicine</a> </span> </div> <div class="col-sm-4 text-right"> <span style="float:right;"> <div class="import_order_mrp">MRP. : <i class="fa fa-inr" aria-hidden="true"></i> '+item_mrp+'/-</div> </span> <span style="float:right; margin-left:10px;margin-right:10px;" class="import_order_mrp"> | </span> <span style="float:right;"> <input type="number" name="item_qty[]" value="'+item_order_quantity+'" class="form-control item_qty_'+js_i+'" style="width: 50px;height: 30px;font-size: 12px;padding: 3px;" onchange="change_order_quantity('+js_i+')" min="1" max="1000" /> </span> <span style="float:right;margin-right:5px;" class="cart_order_qty1"> Quantity : </span> </div> <div class="col-sm-12" style=" border-top-style: solid;border-top-color: #dee2e6;border-top-width: 1px;"></div> </div> <div class="row select_product_'+js_i+'" style="display:none"> <div class="col-sm-8"> DRD :  <span class="import_order_title selected_item_name_'+js_i+'"></span> <span class="import_order_packing"> (<span class="selected_packing_'+js_i+'"></span> Packing) </span> - <span class="import_order_expiry expiry_css_'+js_i+'"> Expiry : <span class="selected_expiry_'+js_i+'"></span></span> </div> <div class="col-sm-4 text-right"> <span class="import_order_stock"> Stock : <span class="selected_batchqty_'+js_i+'"></span></span> | <span class="import_order_mrp"> MRP. : <i class="fa fa-inr" aria-hidden="true"></i> <span class="selected_mrp_'+js_i+'">0.00</span>/- </span> </div>	<div class="col-sm-12" style=" border-top-style: solid;border-top-color: #dee2e6;border-top-width: 1px;"></div> <div class="col-sm-7"> <span class="import_order_company"> By : <span class="selected_company_full_name_'+js_i+'"></span></span> |  <span class="import_order_batch_no"> Batch No : <span class="selected_batch_no_'+js_i+'"></span></span> <span class="select_product_'+js_i+' selected_scheme_span_'+js_i+'"> |  <span class="import_order_scheme selected_scheme_'+js_i+'"></span> </span> </div> <div class="col-sm-5 text-right"> <span class="import_order_ptr"> PTR : <i class="fa fa-inr" aria-hidden="true"></i> <span class="selected_sale_rate_'+js_i+'">0.00</span>/-</span> | <span class="import_order_landing_price"> ~ Landing price : <i class="fa fa-inr" aria-hidden="true"></i> <span class="selected_final_price_'+js_i+'">0.00</span>/-</span> </div> </div> </div> <div class="col-sm-12" style=" border-top-style: solid;border-top-color: #dee2e6;border-top-width: 1px;"></div> <div class="col-sm-12"> <span class="selected_msg_'+js_i+'"> Loading.... </span> <span class="selected_SearchAnotherMedicine_'+js_i+'" style="display:none">  | <a href="'+js1+'" class="cart_delete_btn" title="Change medicine"> <img src="<?= base_url(); ?>img_v51/edit_icon.png" width="18px;" alt="Change medicine"> Change medicine </a> </span> <span class="selected_suggest_'+js_i+'" style="display:none"> | <a href="'+js2+'" title="Delete Suggest Medicine" class="cart_delete_btn"><img src="<?= base_url(); ?>img_v51/delete_icon.png" width="18px;" alt="Delete Suggest Medicine">Delete Suggest Medicine</a> </span> </div> </div> <span class="insert_main_row_data_'+js_i+'"></span> </td> </tr>');
 	setTimeout("insert_main_row_data('"+js_i+"')",1000);
-}
-function delete_medicine(item_code)
-{
-	swal({
-		title: "Are you sure to delete medicine?",
-		/*text: "Once deleted, you will not be able to recover this imaginary file!",*/
-		icon: "warning",
-		buttons: ["No", "Yes"],
-		dangerMode: true,
-	}).then(function(result) {
-		if (result) 
-		{
-			$.ajax({                          
-				url: "<?php echo base_url(); ?>Chemist_json/delete_medicine_api",
-				type:"POST",
-				/*dataType: 'html',*/
-				data: {item_code:item_code},
-				error: function(){
-					swal("Medicine not deleted");
-				},
-				success: function(data){
-					$.each(data.items, function(i,item){	
-						if (item)
-						{
-							if(item.status=="1")
-							{
-								page_load();
-								$(".item_focues"+i_code).html('')
-								swal("Medicine deleted successfully", {
-									icon: "success",
-								});
-							}
-							else{
-								swal("Medicine not deleted");
-							}
-						} 
-					});
-				},
-				timeout: 10000
-			});
-		} else {
-			swal("Medicine not deleted");
-		}
-	});
 }
 function page_load()
 {

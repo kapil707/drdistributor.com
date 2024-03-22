b6e4fcd10899e2f0c0fb8cdb64f325f9bc14c68f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-22 15:38:02 +0530

    add

--- application/views/medicine_search/medicine_search.php
+++ application/views/medicine_search/medicine_search.php
@@ -105,4 +105,4 @@ function goBack() {
 		</div>
 	</div>
 </div>
-<script src="<?php echo base_url(); ?>/assets/js/medicine_search12345678.js"></script>
+<script src="<?php echo base_url(); ?>/assets/js/medicine_search123456789.js"></script>

b6aef26547329a4dd629d871c78913e75a331d2e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-22 14:53:01 +0530

    add

--- application/views/medicine_category/medicine_category.php
+++ application/views/medicine_category/medicine_category.php
@@ -4,7 +4,7 @@ function goBack() {
 	window.location.href = "<?= base_url();?>";
 }
 </script>
-<div class="container-xl main_container">
+<div class="container-lg main_container">
 	<div class="row">
 		<div class="col-sm-12 col-12">
 			<div class="row">

4ea626c4a210a28700616d619c02fced8ebdd574
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-22 18:33:41 +0530

    add

--- assets/js/home_page123456789.js
+++ assets/js/home_page123456789.js
@@ -50,8 +50,8 @@ function home_page_owl_load(type,category_id){
 			autoplay: true, // Enable autoplay
 			autoplayTimeout: 5000, // Autoplay interval in milliseconds
 			responsiveClass:true,
-			navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
 			nav: true,
+			navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
 			responsive:{
 				0:{
 					items:3,

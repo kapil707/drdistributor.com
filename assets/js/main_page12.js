2c48626bbcb5b8fb2364a35c58846cf34eeb92a6
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-23 16:59:35 +0530

    add

--- assets/js/main_page12.js
+++ assets/js/main_page12.js
@@ -531,6 +531,7 @@ function delete_medicine(item_code)
 								}else{
 									order_type = "all";
 								}
+								my_cart_api(order_type);
 								swal("Medicine deleted successfully", {
 									icon: "success",
 								});
@@ -581,6 +582,7 @@ function delete_all_medicine()
 								}else{
 									order_type = "all";
 								}
+								my_cart_api(order_type);
 								swal("Medicines deleted successfully", {
 									icon: "success",
 								});
@@ -657,7 +659,7 @@ function my_cart_api(order_type) {
 					item_expiry 		= item.item_expiry;
 					item_company 		= item.item_company;
 					item_quantity 		= item.item_quantity;
-					item_stock 			= item.item_stock;
+					//item_stock 		= item.item_stock;
 					item_ptr 			= item.item_ptr;
 					item_mrp 			= item.item_mrp;
 					item_price 			= item.item_price;
@@ -669,7 +671,7 @@ function my_cart_api(order_type) {
 					//new add for last order qty
 					item_order_quantity = item.item_order_quantity;
 
-					div_all_data = "<span class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"' item_order_quantity='"+item_order_quantity+"'></span>";
+					div_all_data = "<span class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"' item_order_quantity='"+item_order_quantity+"'></span>";
 
 					item_id 			= item.item_id;
 					item_quantity_price = item.item_quantity_price;

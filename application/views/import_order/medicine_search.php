ae235432c42c41540716895b5f387108825b96dd
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-24 13:16:28 +0530

    add

--- application/views/import_order/medicine_search.php
+++ application/views/import_order/medicine_search.php
@@ -166,7 +166,7 @@ function goBack() {
 get_page_name = "import_page";// change value taki cart pur load na ho 
 order_type = "notall";// change value taki cart pur load na ho 
 </script>
-<script src="<?= base_url(); ?>assets/js/import_order/medicine_search6789t1.js"></script>
+<script src="<?= base_url(); ?>assets/js/import_order/medicine_search789t1.js"></script>
 <script>
 $(document).ready(function(){
 	<?php foreach($result as $row) { ?>

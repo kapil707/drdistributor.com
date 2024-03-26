8d126b0282f74610fe364c31d48433c515da2c6c
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 18:13:50 +0530

    add

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
  */
 	//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-	define('ENVIRONMENT','testing');
+	define('ENVIRONMENT','production');
 
 /*
  *---------------------------------------------------------------

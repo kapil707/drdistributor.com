31b137c183435a7d5c7358a70f1ed153fd77243f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-28 17:46:28 +0530

    add

--- application/views/header_footer/header.php
+++ application/views/header_footer/header.php
@@ -159,9 +159,9 @@ if (isset($_COOKIE["theme_type"])) {
 			
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12">
 				<div class="row">
-					<div class="col-lg-3 col-md-1 col-sm-12 col-xs-12 col-12 text-right">
-						<div class="d-none d-xxl-block wow pulse animated text-right" data-wow-delay="300ms" data-wow-iteration="infinite" data-wow-duration="2s" style="visibility: visible; animation-duration: 2s; animation-delay: 300ms; animation-iteration-count: infinite; animation-name: pulse;">
-							<div class="google_play" style="width: 148px;">
+					<div class="col-lg-3 col-md-1 col-sm-12 col-xs-12 col-12 ">
+						<div class="d-none d-xxl-block wow pulse animated" data-wow-delay="300ms" data-wow-iteration="infinite" data-wow-duration="2s" style="visibility: visible; animation-duration: 2s; animation-delay: 300ms; animation-iteration-count: infinite; animation-name: pulse;">
+							<div class="google_play" style="width: 148px;float: right;">
 								<a href="https://play.google.com/store/apps/details?id=com.drdistributor.dr&hl=en" target="_black" title="Download App">
 									<img src="https://www.drdistributors.co.in/drd-live/img_v51/playstrore.png" alt="Google Play" style="width:35px;">
 									Google Play

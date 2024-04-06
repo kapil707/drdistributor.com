c963973b9a2ce95ebc5c8da174707ee016e5dfd1
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-06 09:58:37 +0530

    add

--- assets/js/main_page1234567.js
+++ assets/js/main_page1234567.js
@@ -454,7 +454,7 @@ function medicine_add_to_cart_api()
 					
 					div_all_data = "<span class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='' item_stock='' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='' item_order_quantity='"+item_order_quantity+"'></span>";
 
-					item_id 			= item.item_id;
+					item_id 			= "0";
 					item_quantity_price = parseFloat(item_order_quantity) * parseFloat(item_price);
 					item_datetime 		= "Just now";
 					item_modalnumber 	= "PC / Laptop";

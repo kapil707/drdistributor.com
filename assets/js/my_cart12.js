2548075d1b7f0faaa6fb2a624f1f087fbbaf3405
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-08 10:00:55 +0530

    add

--- assets/js/my_cart12.js
+++ assets/js/my_cart12.js
@@ -57,10 +57,9 @@ function place_order_complete()
 					status_message = (item.status_message);
 					if(status=="0" || status=="1")
 					{
-						$(".place_order_div").hide();
-						$(".main_container").html("<h1 class='text-center'>"+status_message+"</h1><h1 class='text-center'><input type='submit' value='Go home' class='btn main_theme_button' name='Go home' onclick='gohome()' style='width:50%;margin-top:100px;'></h1>");
+						$(".place_order_div").html("<h1 class='text-center'>"+status_message+"</h1><h1 class='text-center'><input type='submit' value='Go home' class='btn main_theme_button' name='Go home' onclick='gohome()' style='width:50%;margin-top:100px;'></h1>");
 				    }
-					/******************************** */
+					/******************************** *
 					get_my_cart_total_api();
 					/******************************** */
 				}

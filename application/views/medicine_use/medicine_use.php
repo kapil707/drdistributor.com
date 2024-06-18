3c886af6aa0a5f3a17bf0218326d42c83cb41a69
kapilji <kapil707sharma@gmail.com>
2024-06-18 14:12:47 +0530

    add

--- application/views/medicine_use/medicine_use.php
+++ application/views/medicine_use/medicine_use.php
@@ -205,89 +205,87 @@ function call_page(get_record)
 					$(".load_page_loading").html("");
 				}
 				console.log(data.items);
-				$.each(data.result, function(i,row){
-					/*$.each(row.medicine_details, function(i,item){
-						$(".headertitle").html(item.item_name);						
+				/*$.each(row.medicine_details, function(i,item){
+					$(".headertitle").html(item.item_name);						
+					
+					$(".medicine_details_item_name").html(item.item_name);
+					$(".medicine_details_item_packing").html("Packing : "+item.item_packing)
+					$(".medicine_details_item_batch_no").html("Batch no : "+item.item_batch_no)
+					$(".medicine_details_item_margin").html(item.item_margin+'% Margin')
+					$(".medicine_details_item_expiry").html("Expiry : "+item.item_expiry)
+					$(".medicine_details_item_company").html("By "+item.item_company)
+					$(".medicine_details_item_stock").html("Stock : " +item.item_quantity)
+					$(".medicine_details_item_scheme").html("Scheme : " +item.item_scheme)
+					$(".medicine_details_item_description1").html(item.item_description1)
+					$(".medicine_details_item_description1").show()
+					
+					if(item.item_description1=="")
+					{
+						$(".medicine_details_item_description1").hide()
+					}
+					$(".medicine_details_item_ptr").html('PTR : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_ptr + "/-")
+					$(".medicine_details_item_mrp").html('MRP : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_mrp + "/-")
+					$(".medicine_details_item_gst").html("GST : "+item.item_gst +"%")
+					$(".medicine_details_item_price").html('*Approximate Value ~ : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_price + "/-")
+					$(".medicine_details_item_scheme_line").show()
+					$(".medicine_details_item_scheme").show()
+					if(item.item_scheme=="0+0")
+					{
+						$(".medicine_details_out_of_stock_img").hide()
+						$(".medicine_details_item_scheme_line").hide()
+						$(".medicine_details_item_scheme").hide()
+					}
+					if(item.item_featured=="1" && item.item_quantity!="0"){
+						$(".medicine_details_featured_img").show()
+					}
+					if(parseInt(item.item_quantity)==0){
 						
-						$(".medicine_details_item_name").html(item.item_name);
-						$(".medicine_details_item_packing").html("Packing : "+item.item_packing)
-						$(".medicine_details_item_batch_no").html("Batch no : "+item.item_batch_no)
-						$(".medicine_details_item_margin").html(item.item_margin+'% Margin')
-						$(".medicine_details_item_expiry").html("Expiry : "+item.item_expiry)
-						$(".medicine_details_item_company").html("By "+item.item_company)
-						$(".medicine_details_item_stock").html("Stock : " +item.item_quantity)
-						$(".medicine_details_item_scheme").html("Scheme : " +item.item_scheme)
-						$(".medicine_details_item_description1").html(item.item_description1)
-						$(".medicine_details_item_description1").show()
+						$(".medicine_details_item_add_to_cart_btn_disable").show()
+						$(".medicine_details_item_stock").html("<font color=red>Out of stock</font>")
+						$(".medicine_details_out_of_stock_img").show()
+						$(".medicine_details_item_scheme").hide()
+						$(".medicine_details_item_scheme_line").hide()
+					}else{
+						$(".medicine_details_item_add_to_cart_btn").show()
+					}
+					if(item.item_stock!="")
+					{
+						$(".medicine_details_item_stock").html(item.item_stock)
+					}
+					$(".medicine_details_item_quantity").val(item.item_quantity)
+					if(item.item_order_quantity){
+						$(".medicine_details_item_order_quantity_textbox").val(item.item_order_quantity)
+					}
+					$(".medicine_details_item_order_quantity_textbox").focus()
+					
+					$(".medicine_details_image").attr("src",item.item_image)
+					$(".medicine_details_image_small").attr("src",item.item_image)
+				});*/
+				$.each(row.medicine_use, function(i,item){
+					if (item){
+						file			= item.file;
+						file_type		= item.file_type;
 						
-						if(item.item_description1=="")
-						{
-							$(".medicine_details_item_description1").hide()
+						image = video = "";
+						if(file_type=="image"){
+							image = '<img src="'+file+'" width="100%">';
 						}
-						$(".medicine_details_item_ptr").html('PTR : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_ptr + "/-")
-						$(".medicine_details_item_mrp").html('MRP : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_mrp + "/-")
-						$(".medicine_details_item_gst").html("GST : "+item.item_gst +"%")
-						$(".medicine_details_item_price").html('*Approximate Value ~ : <i class="fa fa-inr" aria-hidden="true"></i> ' +item.item_price + "/-")
-						$(".medicine_details_item_scheme_line").show()
-						$(".medicine_details_item_scheme").show()
-						if(item.item_scheme=="0+0")
-						{
-							$(".medicine_details_out_of_stock_img").hide()
-							$(".medicine_details_item_scheme_line").hide()
-							$(".medicine_details_item_scheme").hide()
-						}
-						if(item.item_featured=="1" && item.item_quantity!="0"){
-							$(".medicine_details_featured_img").show()
-						}
-						if(parseInt(item.item_quantity)==0){
-							
-							$(".medicine_details_item_add_to_cart_btn_disable").show()
-							$(".medicine_details_item_stock").html("<font color=red>Out of stock</font>")
-							$(".medicine_details_out_of_stock_img").show()
-							$(".medicine_details_item_scheme").hide()
-							$(".medicine_details_item_scheme_line").hide()
-						}else{
-							$(".medicine_details_item_add_to_cart_btn").show()
-						}
-						if(item.item_stock!="")
-						{
-							$(".medicine_details_item_stock").html(item.item_stock)
-						}
-						$(".medicine_details_item_quantity").val(item.item_quantity)
-						if(item.item_order_quantity){
-							$(".medicine_details_item_order_quantity_textbox").val(item.item_order_quantity)
-						}
-						$(".medicine_details_item_order_quantity_textbox").focus()
 						
-						$(".medicine_details_image").attr("src",item.item_image)
-						$(".medicine_details_image_small").attr("src",item.item_image)
-					});*/
-					$.each(row.medicine_use, function(i,item){
-						if (item){
-							file			= item.file;
-							file_type		= item.file_type;
-							
-							image = video = "";
-							if(file_type=="image"){
-								image = '<img src="'+file+'" width="100%">';
-							}
-							
-							if(file_type=="video"){
-								video = '<video width="100%" height="250" controls="" poster="<?php echo base_url(); ?>img_v51/default-video-thumbnail.jpg"><source src="'+file+'" type="video/mp4">Your browser does not support the video tag.</video>';
-							}
-							
-							if(image!=""){
-								$(".load_page_images").append('<div class="col-sm-2 col-6 p-0 m-0 text-center"><div class="medicine_use_div">'+image+'</div></div>');
-							}
-							if(video!=""){
-								$(".load_page_videos").append('<div class="col-sm-6 col-6 p-0 m-0 text-center"><div class="medicine_use_div1">'+video+'</div></div>');
-							}
-							//$(".headertitle").html(item.item_header_title);
-							query_work = 0;
-							no_record_found = 1;
-							$(".load_more").show();
+						if(file_type=="video"){
+							video = '<video width="100%" height="250" controls="" poster="<?php echo base_url(); ?>img_v51/default-video-thumbnail.jpg"><source src="'+file+'" type="video/mp4">Your browser does not support the video tag.</video>';
 						}
-					});
+						
+						if(image!=""){
+							$(".load_page_images").append('<div class="col-sm-2 col-6 p-0 m-0 text-center"><div class="medicine_use_div">'+image+'</div></div>');
+						}
+						if(video!=""){
+							$(".load_page_videos").append('<div class="col-sm-6 col-6 p-0 m-0 text-center"><div class="medicine_use_div1">'+video+'</div></div>');
+						}
+						//$(".headertitle").html(item.item_header_title);
+						query_work = 0;
+						no_record_found = 1;
+						$(".load_more").show();
+					}
 				});
 			},
 			timeout: 10000

86dd7d0dc5343e22c3697569badc96653eaf0227
kapilji <kapil707sharma@gmail.com>
2024-10-09 10:27:53 +0530

    add

--- application/models/model-drdistributor/my_invoice/MyInvoiceModel.php
+++ application/models/model-drdistributor/my_invoice/MyInvoiceModel.php
@@ -90,7 +90,7 @@ class MyInvoiceModel extends CI_Model
 		$order_by = array('id','desc');
 		//$get_limit = array('12',$get_record);
 		$get_limit = array($limit,$get_record);
-		$where = array('chemist_id'=>$user_altercode,'vdt <=', '2024-09-30');
+		$where = array('chemist_ids'=>$user_altercode,'vdt <=', '2024-09-30');
 		$query = $this->select_fun_limit("tbl_invoice_new",$where,$get_limit,$order_by);
 		$query = $query->result();
 		foreach($query as $row)

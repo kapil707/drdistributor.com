0d19aa72c897a5d7a1189eaaf61bab595c3b65ea
kapilji <kapil707sharma@gmail.com>
2024-10-10 14:28:00 +0530

    add

--- application/models/model-drdistributor/my_invoice/MyInvoiceModel.php
+++ application/models/model-drdistributor/my_invoice/MyInvoiceModel.php
@@ -432,7 +432,7 @@ class MyInvoiceModel extends CI_Model
 		$objPHPExcel->getActiveSheet()->getStyle('A1:AG1')->applyFromArray($BStyle);
 		
 		/**********************************************/
-		$this->db->select('tbl_chemist.name as chemist_name, tbl_medicine.item_name, tbl_medicine.item_code, tbl_medicine.packing,tbl_medicine.batch_no, tbl_medicine.company_full_name,tbl_medicine.altercode as chemist_id, tbl_invoice_item.*');
+		$this->db->select('tbl_chemist.name as chemist_name, tbl_medicine.item_name, tbl_medicine.item_code, tbl_medicine.packing,tbl_medicine.batch_no, tbl_medicine.company_full_name, tbl_invoice_item.*');
         $this->db->from('tbl_invoice_item');
         $this->db->join('tbl_invoice', 'tbl_invoice.vno = tbl_invoice_item.vno AND tbl_invoice.date = tbl_invoice_item.date', 'left');
         $this->db->join('tbl_chemist', 'tbl_chemist.altercode = tbl_invoice.chemist_id', 'left');
@@ -481,7 +481,7 @@ class MyInvoiceModel extends CI_Model
 			$objPHPExcel->getActiveSheet()->SetCellValue('AD'.$rowCount,$row->psrlno);
 			$objPHPExcel->getActiveSheet()->SetCellValue('AE'.$rowCount,"0");
 			$objPHPExcel->getActiveSheet()->SetCellValue('AF'.$rowCount,"0");
-			$objPHPExcel->getActiveSheet()->SetCellValue('AG'.$rowCount,$chemist_id);
+			$objPHPExcel->getActiveSheet()->SetCellValue('AG'.$rowCount,$row->chemist_id);
 			
 			
 			$item_name  = $row->item_name;

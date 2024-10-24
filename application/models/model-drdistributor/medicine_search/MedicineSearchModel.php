c7b8de58cde6e64bacd48e53c9b7ba81180bd7c3
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-24 11:40:13 +0530

    add

--- application/models/model-drdistributor/medicine_search/MedicineSearchModel.php
+++ application/models/model-drdistributor/medicine_search/MedicineSearchModel.php
@@ -53,13 +53,10 @@ class MedicineSearchModel extends CI_Model
 		if ($checkbox_medicine == 0 && $checkbox_company == 1) {
 			$this->db->like('company_full_name', $keyword_item_name);
 		}
-
-		if ($checkbox_medicine == 0 && $checkbox_company == 0) {
-			$this->db->like('packing', $keyword_item_name, 'both');
-			$this->db->or_like('batch_no', $keyword_item_name, 'both');
-			$this->db->or_like('title2', $keyword_item_name, 'both');
-			$this->db->or_like('description', $keyword_item_name, 'both');
-		}
+		$this->db->or_like('packing', $keyword_item_name, 'both');
+		$this->db->or_like('batch_no', $keyword_item_name, 'both');
+		$this->db->or_like('title2', $keyword_item_name, 'both');
+		$this->db->or_like('description', $keyword_item_name, 'both');
 		foreach($keyword_array as $row_val){
 			//$this->db->or_like('item_name', $row_val);
 		}

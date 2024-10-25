cfee671e3bdb0d19afba554c3138df63413d4af2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-25 17:53:54 +0530

    add

--- application/models/model-drdistributor/medicine_search/MedicineSearchModel.php
+++ application/models/model-drdistributor/medicine_search/MedicineSearchModel.php
@@ -527,5 +527,19 @@ class MedicineSearchModel extends CI_Model
 		);
 		return $dt;
 	}
+
+	public function log_search_activity($chemist_id, $salesman_id, $search_term="", $product_viewed="") {
+        $data = array(
+            'chemist_id' => $chemist_id,
+            'salesman_id' => $salesman_id,
+            'search_term' => $search_term,
+            'product_viewed' => $product_viewed,
+            'date' => date('Y-m-d'),
+            'time' => date('H:i:s'),
+            'timestamp' => time(),
+        );
+
+        $this->db->insert('tbl_search_logs', $data);
+    }
 }
 

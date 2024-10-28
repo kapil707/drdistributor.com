ef9776c217981976bf9bfa6c7cddc57e2750a1c5
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-28 16:36:41 +0530

    add

---
+++ application/controllers/My_broadcast.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class My_broadcast extends CI_Controller {
+	
+	public function __construct(){
+		parent::__construct();
+		// Load model
+
+		$this->load->model("model-drdistributor/my_broadcast/MyBroadcastModel");
+		/***********************log file start*************************** */
+		if(!empty($_COOKIE["user_altercode"])){
+			$user_type 		= $_COOKIE["user_type"];
+			$user_altercode = $_COOKIE["user_altercode"];
+
+			$chemist_id = $salesman_id = "";
+			if($user_type=="sales")
+			{
+				$chemist_id 	= $_COOKIE["chemist_id"];
+				$salesman_id 	= $user_altercode;
+				$user_altercode = $chemist_id;
+			}
+			//logs create from hear
+			log_activity($user_altercode,$salesman_id,$user_type,"web");
+		}
+		/***********************log file end*************************** */
+	}
+
+	/*******************api start*********************/
+	public function my_broadcast_api(){
+		$get_record	 	= $_REQUEST["get_record"];
+		$user_type 		= $_COOKIE["user_type"];
+		$user_altercode = $_COOKIE["user_altercode"];
+		$user_password	= $_COOKIE["user_password"];
+		$chemist_id 	= "";
+		$salesman_id = "";
+		if($user_type=="sales")
+		{
+			$chemist_id 	= $_COOKIE["chemist_id"];
+			$salesman_id 	= $user_altercode;
+			$user_altercode = $chemist_id;
+		}
+		$items = "";
+		if(!empty($user_type) && !empty($user_altercode)) {
+
+			$result = $this->MyBroadcastModel->get_my_nbroadcast_api($user_type,$user_altercode,$salesman_id);
+			$items  	= $result["items"];
+		}
+
+		$response = array(
+            'success' => "1",
+            'message' => 'Data load successfully',
+            'items' => $items
+        );
+
+        // Send JSON response
+        header('Content-Type: application/json');
+        echo json_encode($response);
+	}
+}
---
+++ application/models/model-drdistributor/my_broadcast/MyBroadcastModel.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class MyBroadcastModel extends CI_Model  
+{
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function get_my_broadcast_api($user_type="",$user_altercode="",$salesman_id="") {
+				
+		$jsonArray = array();
+		
+		$this->db->where('chemist_id',$user_altercode);
+		$this->db->where('status',0);
+		$this->db->order_by('id','desc');
+		$this->db->limit(1);
+		$query = $this->db->get("tbl_broadcast")->result();
+		foreach($query as $row)
+		{
+			$get_record++;
+			$item_id		=	$row->id;
+			$item_title		=	($row->title);
+			$item_message	=	($row->message);
+			$item_message	=   str_replace("<br>"," ",$item_message);
+			$item_date_time = 	date("d-M-y @ h:i A", $row->timestamp);
+			//$item_message	= 	substr($item_message, 0, 50)."....";
+			
+			$dt 	= array('status'=>1);
+			$where 	= array('id'=>$item_id);
+			$this->Scheme_Model->edit_fun("tbl_broadcast",$dt,$where);
+
+			$item_image = "";
+			
+			$dt = array(
+				'item_id' => $item_id,
+				'item_title' => $item_title,
+				'item_message' => $item_message,
+				'item_date_time' => $item_date_time,
+				'item_image' => $item_image,
+			);
+			$jsonArray[] = $dt;
+		}
+		//$jsonString  = json_encode($jsonArray);
+		
+		$return["items"] 		= $jsonArray;
+		$return["get_record"] 	= $get_record;
+		return $return;
+	}
+}

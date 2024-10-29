3374cae3549446dd84739e5289372625d47b9c28
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-29 14:53:39 +0530

    add

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -56,7 +56,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 //$autoload['libraries'] = array('globals','database', 'email', 'session', 'form_validation','breadcrumbs','pagination');
-$autoload['libraries'] = array('database', 'session','AppConfig');
+$autoload['libraries'] = array('database', 'session');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Drivers

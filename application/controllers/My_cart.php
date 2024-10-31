15f186688f72458b1eae609ad4b7b1447ce21548
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-31 12:51:41 +0530

    add

--- application/controllers/My_cart.php
+++ application/controllers/My_cart.php
@@ -2,15 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class My_cart extends CI_Controller {
 
-	var $user_image = "";
-	var $user_fname = "";
-	var $delivering_to = "";
-	var $user_type = "";
-	var $user_altercode = "";
-	var $user_password = "";
-	var $chemist_id = "";
-	var $salesman_id = "";
-	var $user_nrx  = "";
+	var $UserId 		= "";
+	var $UserType 		= "";
+	var $UserFullName 	= "";
+	var $UserPassword 	= "";
+	var $UserImage 		= "";
+	var $ChemistNrx 	= "";
+	var $ChemistId 		= "";
+	var $SalesmanId 	= "";
 	
 	public function __construct(){
 		parent::__construct();
@@ -30,23 +29,14 @@ class My_cart extends CI_Controller {
 		$this->load->model("model-drdistributor/my_cart/MyCartModel");
 
 		/********************session start***************************** */
-		$this->user_image 	 = $this->session->userdata('user_image');
-		$this->user_fname    = $this->session->userdata('user_fname');
-		$this->delivering_to = $this->session->userdata('user_altercode');	
-		
-		$this->user_type 		= $this->session->userdata('user_type');
-		$this->user_altercode 	= $this->session->userdata('user_altercode');
-		$this->user_password	= $this->session->userdata('user_password');
-		$this->user_nrx			= $this->session->userdata('user_nrx');
-
-		$chemist_id = $salesman_id = "";
-		if($this->user_type=="sales" && !empty($this->session->userdata('chemist_id')))
-		{
-			$this->chemist_id 		= $this->session->userdata('chemist_id');
-			$this->salesman_id 		= $this->user_altercode;
-			$this->user_altercode 	= $this->chemist_id;
-			$this->delivering_to 	= $this->chemist_id;
-		}
+		$this->UserId		= $this->session->userdata('UserId');
+		$this->UserType    	= $this->session->userdata('UserType');
+		$this->UserFullName = $this->session->userdata('UserFullName');
+		$this->UserPassword	= $this->session->userdata('UserPassword');
+		$this->UserImage 	= $this->session->userdata('UserImage');
+		$this->ChemistNrx	= $this->session->userdata('ChemistNrx');
+		$this->ChemistId	= $this->session->userdata('ChemistId');
+		$this->SalesmanId	= $this->session->userdata('SalesmanId');
 		/********************************************************** */
 	}
 
@@ -58,16 +48,20 @@ class My_cart extends CI_Controller {
 		/********************************************************** */
 
 		/********************PageMainData************************** */
-		$data["session_user_type"] 		= $this->user_type;
-		$data["session_user_image"] 	= $this->user_image;
-		$data["session_user_fname"]     = $this->user_fname;
-		$data["session_user_altercode"] = $this->user_altercode;
-		$data["session_delivering_to"]  = $this->delivering_to;
+		$data["UserId"] 	 = $this->UserId;
+		$data["UserType"]    = $this->UserType;
+		$data["UserFullName"]= $this->UserFullName;
+		$data["UserPassword"]= $this->UserPassword;
+		$data["UserImage"] 	 = $this->UserImage;
+		$data["ChemistNrx"]	 = $this->ChemistNrx;
+		$data["ChemistId"]	 = $this->ChemistId;
+		$data["SalesmanId"]	 = $this->SalesmanId;
 
-		$data["chemist_id"] = $chemist_id = $this->chemist_id; 
-		if($this->user_type=="sales")
+		/******************DeliveringToData************************* */
+		$data["DeliveringTo"]= $data["ChemistId"];
+		if($this->UserType=="sales")
 		{
-			$data["session_delivering_to"] = $chemist_id." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
+			$data["DeliveringTo"] = $data["ChemistId"]." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
 		}
 		/********************************************************** */
 		
@@ -77,15 +71,15 @@ class My_cart extends CI_Controller {
 
 	/*******************api start*********************/
 	public function my_cart_total_api(){
-		$user_type 		= $this->user_type;
-		$user_altercode = $this->user_altercode;
-		$chemist_id 	= $this->chemist_id;
-		$salesman_id 	= $this->salesman_id;
-		/********************************************************** */
+
+		$UserType 		= $this->UserType;
+		$ChemistId 		= $this->ChemistId;
+		$SalesmanId 	= $this->SalesmanId;
+		
 		$items = "";
-		if(!empty($user_altercode))
-		{
-			$result = $this->MyCartModel->my_cart_total_api($user_type,$user_altercode,$user_password,$salesman_id);
+		if(!empty($UserType) && !empty($ChemistId))	{
+
+			$result = $this->MyCartModel->my_cart_total_api($UserType,$ChemistId,$user_password,$SalesmanId);
 			$items = $result["items"];
 		}
 		
@@ -101,17 +95,17 @@ class My_cart extends CI_Controller {
 	}
 	public function my_cart_api(){
 		
-		$user_type 		= $this->user_type;
-		$user_altercode = $this->user_altercode;
-		$user_password	= $this->user_password;
-		$chemist_id 	= $this->chemist_id;
-		$salesman_id 	= $this->salesman_id;
-		/********************************************************** */
+		$UserType 		= $this->UserType;
+		$ChemistId 		= $this->ChemistId;
+		$SalesmanId 	= $this->SalesmanId;
+		$UserPassword 	= $this->UserPassword;
+		
 		$order_type = $_POST["order_type"];
 		$items = $items_other = "";
-		if(!empty($user_altercode))
-		{
-			$result = $this->MyCartModel->my_cart_api($user_type,$user_altercode,$user_password,$salesman_id,$order_type);
+		if(!empty($UserType) && !empty($ChemistId))	{
+
+			$result = $this->MyCartModel->my_cart_api($UserType,$ChemistId,$UserPassword,$SalesmanId,$order_type);
+
 			$items = $result["items"];
 			$items_other = $result["items_other"];
 			$items_total = $result["items_total"];
@@ -131,6 +125,10 @@ class My_cart extends CI_Controller {
 
 	public function medicine_add_to_cart_api()
 	{
+		$UserType 		= $this->UserType;
+		$ChemistId 		= $this->ChemistId;
+		$SalesmanId 	= $this->SalesmanId;
+
 		$item_code				= $_REQUEST["item_code"];
 		$item_order_quantity	= $_REQUEST["item_order_quantity"];
 		$order_type 	= "pc_mobile";
@@ -138,15 +136,11 @@ class My_cart extends CI_Controller {
 		$modalnumber 	= "PC / Laptop";
 		$device_id 		= "";
 		
-		$user_type 		= $this->user_type;
-		$user_altercode = $this->user_altercode;
-		$chemist_id 	= $this->chemist_id;
-		$salesman_id 	= $this->salesman_id;
-		
 		$status = $status_message = "";
-		if(!empty($user_type) && !empty($user_altercode)){
-			$excel_number = "";		
-			$result = $this->MyCartModel->medicine_add_to_cart_api($user_type,$user_altercode,$salesman_id,$order_type,$item_code,$item_order_quantity,$mobilenumber,$modalnumber,$device_id,$excel_number);
+		if(!empty($UserType) && !empty($ChemistId)){
+			
+			$excel_number = "";
+			$result = $this->MyCartModel->medicine_add_to_cart_api($UserType,$ChemistId,$SalesmanId,$order_type,$item_code,$item_order_quantity,$mobilenumber,$modalnumber,$device_id,$excel_number);
 			
 			$status = $result["status"];
 			$status_message = $result["status_message"];

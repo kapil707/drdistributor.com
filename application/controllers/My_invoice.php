dff1cbd90920f85b1128413a6745e78c26f2e0ec
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-31 12:15:15 +0530

    add

--- application/controllers/My_invoice.php
+++ application/controllers/My_invoice.php
@@ -2,15 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class My_invoice extends CI_Controller {
 
-	var $user_image = "";
-	var $user_fname = "";
-	var $delivering_to = "";
-	var $user_type = "";
-	var $user_altercode = "";
-	var $user_password = "";
-	var $chemist_id = "";
-	var $salesman_id = "";
-	var $user_nrx  = "";
+	var $UserId 		= "";
+	var $UserType 		= "";
+	var $UserFullName 	= "";
+	var $UserPassword 	= "";
+	var $UserImage 		= "";
+	var $ChemistNrx 	= "";
+	var $ChemistId 		= "";
+	var $SalesmanId 	= "";
 	
 	public function __construct(){
 		parent::__construct();
@@ -26,23 +25,14 @@ class My_invoice extends CI_Controller {
 		$this->load->model("model-drdistributor/my_invoice/MyInvoiceModel");
 
 		/********************session start***************************** */
-		$this->user_image 	 = $this->session->userdata('user_image');
-		$this->user_fname    = $this->session->userdata('user_fname');
-		$this->delivering_to = $this->session->userdata('user_altercode');	
-		
-		$this->user_type 		= $this->session->userdata('user_type');
-		$this->user_altercode 	= $this->session->userdata('user_altercode');
-		$this->user_password	= $this->session->userdata('user_password');
-		$this->user_nrx			= $this->session->userdata('user_nrx');
-
-		$chemist_id = $salesman_id = "";
-		if($this->user_type=="sales" && !empty($this->session->userdata('chemist_id')))
-		{
-			$this->chemist_id 		= $this->session->userdata('chemist_id');
-			$this->salesman_id 		= $this->user_altercode;
-			$this->user_altercode 	= $this->chemist_id;
-			$this->delivering_to 	= $this->chemist_id;
-		}
+		$this->UserId		= $this->session->userdata('UserId');
+		$this->UserType    	= $this->session->userdata('UserType');
+		$this->UserFullName = $this->session->userdata('UserFullName');
+		$this->UserPassword	= $this->session->userdata('UserPassword');
+		$this->UserImage 	= $this->session->userdata('UserImage');
+		$this->ChemistNrx	= $this->session->userdata('ChemistNrx');
+		$this->ChemistId	= $this->session->userdata('ChemistId');
+		$this->SalesmanId	= $this->session->userdata('SalesmanId');
 		/********************************************************** */
 	}
 
@@ -57,16 +47,20 @@ class My_invoice extends CI_Controller {
 		/********************************************************** */
 
 		/********************PageMainData************************** */
-		$data["session_user_type"] 		= $this->user_type;
-		$data["session_user_image"] 	= $this->user_image;
-		$data["session_user_fname"]     = $this->user_fname;
-		$data["session_user_altercode"] = $this->user_altercode;
-		$data["session_delivering_to"]  = $this->delivering_to;
+		$data["UserId"] 	 = $this->UserId;
+		$data["UserType"]    = $this->UserType;
+		$data["UserFullName"]= $this->UserFullName;
+		$data["UserPassword"]= $this->UserPassword;
+		$data["UserImage"] 	 = $this->UserImage;
+		$data["ChemistNrx"]	 = $this->ChemistNrx;
+		$data["ChemistId"]	 = $this->ChemistId;
+		$data["SalesmanId"]	 = $this->SalesmanId;
 
-		$data["chemist_id"] = $chemist_id = $this->chemist_id; 
-		if($this->user_type=="sales")
+		/******************DeliveringToData************************* */
+		$data["DeliveringTo"]= $data["ChemistId"];
+		if($this->UserType=="sales")
 		{
-			$data["session_delivering_to"] = $chemist_id." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
+			$data["DeliveringTo"] = $data["ChemistId"]." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
 		}
 		/********************************************************** */
 
@@ -87,16 +81,20 @@ class My_invoice extends CI_Controller {
 		/********************************************************** */
 
 		/********************PageMainData************************** */
-		$data["session_user_type"] 		= $this->user_type;
-		$data["session_user_image"] 	= $this->user_image;
-		$data["session_user_fname"]     = $this->user_fname;
-		$data["session_user_altercode"] = $this->user_altercode;
-		$data["session_delivering_to"]  = $this->delivering_to;
+		$data["UserId"] 	 = $this->UserId;
+		$data["UserType"]    = $this->UserType;
+		$data["UserFullName"]= $this->UserFullName;
+		$data["UserPassword"]= $this->UserPassword;
+		$data["UserImage"] 	 = $this->UserImage;
+		$data["ChemistNrx"]	 = $this->ChemistNrx;
+		$data["ChemistId"]	 = $this->ChemistId;
+		$data["SalesmanId"]	 = $this->SalesmanId;
 
-		$data["chemist_id"] = $chemist_id = $this->chemist_id; 
-		if($this->user_type=="sales")
+		/******************DeliveringToData************************* */
+		$data["DeliveringTo"]= $data["ChemistId"];
+		if($this->UserType=="sales")
 		{
-			$data["session_delivering_to"] = $chemist_id." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
+			$data["DeliveringTo"] = $data["ChemistId"]." | <a href='".base_url()."select_chemist' class='all_chemist_edit_btn'> <i class='fa fa-pencil all_chemist_edit_btn' aria-hidden='true'></i> Edit chemist</a>";
 		}
 		/********************************************************** */
 
@@ -110,16 +108,15 @@ class My_invoice extends CI_Controller {
 	/*******************api start*********************/
 	public function my_invoice_api(){
 
-		$user_type 		= $this->user_type;
-		$user_altercode = $this->user_altercode;
-		$chemist_id 	= $this->chemist_id;
-		$salesman_id 	= $this->salesman_id;
+		$UserType 		= $this->UserType;
+		$ChemistId 		= $this->ChemistId;
+		$SalesmanId 	= $this->SalesmanId;
 
 		$get_record	 	= $_REQUEST["get_record"];
 		$items = "";
-		if(!empty($user_type) && !empty($user_altercode)) {
+		if(!empty($UserType) && !empty($ChemistId)) {
 
-			$result = $this->MyInvoiceModel->get_my_invoice_api($user_type,$user_altercode,$salesman_id,$get_record);
+			$result = $this->MyInvoiceModel->get_my_invoice_api($UserType,$ChemistId,$SalesmanId,$get_record);
 			$items  	= $result["items"];
 			$get_record  = $result["get_record"];
 		}
@@ -137,15 +134,16 @@ class My_invoice extends CI_Controller {
 	}
 
 	public function my_invoice_details_api(){
-		$user_type 		= $this->user_type;
-		$user_altercode = $this->user_altercode;
-		$chemist_id 	= $this->chemist_id;
-		$salesman_id 	= $this->salesman_id;
+		
+		$UserType 		= $this->UserType;
+		$ChemistId 		= $this->ChemistId;
+		$SalesmanId 	= $this->SalesmanId;
 
 		$item_id		= $_REQUEST['item_id'];
 		$items = $items_edit = $items_delete = $download_url = $title = "";
-		if(!empty($user_type) && !empty($user_altercode) && !empty($item_id)){			
-			$result = $this->MyInvoiceModel->get_my_invoice_details_api($user_type,$user_altercode,$salesman_id,$item_id);
+		if(!empty($UserType) && !empty($ChemistId) && !empty($item_id)){
+
+			$result = $this->MyInvoiceModel->get_my_invoice_details_api($UserType,$ChemistId,$SalesmanId,$item_id);
 			$items  		= $result["items"];
 			$items_edit  	= $result["items_edit"];
 			$items_delete  	= $result["items_delete"];

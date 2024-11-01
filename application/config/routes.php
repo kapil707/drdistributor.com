50321d3596bcda3f15473208e07acb52ab8d4043
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-01 11:54:27 +0530

    add

--- application/config/routes.php
+++ application/config/routes.php
@@ -96,6 +96,7 @@ $route['medicine_use/(:any)']='medicine_use/index/$1';
 $route['my_order']='my_order/index';
 $route['mo']='my_order/index';
 $route['my_order_details/(:any)']='my_order/my_order_details/$1';
+$route['mod/(:any)']='my_order/my_order_details/$1';
 $route['user/download_order/(:any)/(:any)'] = 'main/download_order_old/$1/$2';
 $route['order_download/(:any)/(:any)'] = 'main/order_download/$1/$2';
 $route['od/(:any)/(:any)'] = 'main/order_download/$1/$2';

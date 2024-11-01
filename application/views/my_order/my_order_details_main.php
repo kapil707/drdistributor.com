73b237b596a6bdc36037385064ed0763fdaa06eb
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-01 08:37:28 +0530

    add

--- application/views/my_order/my_order_details_main.php
+++ application/views/my_order/my_order_details_main.php
@@ -23,4 +23,4 @@ function goBack() {
 var item_id = "<?php echo $item_id; ?>";
 var ChemistId = "<?php echo $ChemistId; ?>";
 </script>
-<script src="<?php echo base_url(); ?>/assets/js-<?php echo $this->appconfig->getWebJs(); ?>/my_order_details_main.js"></script>
+<script src="<?php echo base_url(); ?>/assets/js-<?php echo $this->appconfig->getWebJs(); ?>/my_order_details_main1.js"></script>

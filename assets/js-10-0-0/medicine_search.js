7c7e4715cdb3e4716670ed243de0f0e8c0a389bd
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-11-01 08:59:18 +0530

    add

--- assets/js-10-0-0/medicine_search.js
+++ assets/js-10-0-0/medicine_search.js
@@ -242,6 +242,7 @@ function medicine_search_api() {
 							item_image			= item.item_image;
 							item_name 			= item.item_name;
 							item_packing 		= item.item_packing;
+							item_batch_no		= "Loading...";
 							item_expiry 		= item.item_expiry;
 							item_company 		= item.item_company;
 							item_quantity 		= item.item_quantity;
@@ -249,13 +250,14 @@ function medicine_search_api() {
 							item_ptr 			= item.item_ptr;
 							item_mrp 			= item.item_mrp;
 							item_price 			= item.item_price;
+							item_gst 			= "Loading...";
 							item_scheme 		= item.item_scheme;
 							item_margin 		= item.item_margin;
 							item_featured 		= item.item_featured;
 							item_description1 	= item.item_description;
 							similar_items 		= item.similar_items;
 
-							div_all_data = "<div class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></div>"
+							div_all_data = "<div class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_batch_no='"+item_batch_no+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_gst='"+item_gst+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></div>"
 							
 							item_scheme_div = "";
 							if(item_scheme!="0+0")

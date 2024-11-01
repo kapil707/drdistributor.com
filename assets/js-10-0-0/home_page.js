179b1fcd2416788c280a701687e8bfd6f81700c6
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-11-01 09:17:33 +0530

    add

--- assets/js-10-0-0/home_page.js
+++ assets/js-10-0-0/home_page.js
@@ -351,6 +351,7 @@ function home_page_itemcategory(category_id,items,title){
 			item_image			= item.item_image;
 			item_name 			= item.item_name;
 			item_packing 		= item.item_packing;
+			item_batch_no		= "xxxxxx";
 			item_expiry 		= item.item_expiry;
 			item_company 		= item.item_company;
 			item_quantity 		= item.item_quantity;
@@ -358,6 +359,7 @@ function home_page_itemcategory(category_id,items,title){
 			item_ptr 			= item.item_ptr;
 			item_mrp 			= item.item_mrp;
 			item_price 			= item.item_price;
+			item_gst 			= "0";
 			item_scheme 		= item.item_scheme;
 			item_margin 		= item.item_margin;
 			item_featured 		= item.item_featured;
@@ -366,8 +368,7 @@ function home_page_itemcategory(category_id,items,title){
 			similar_items 		= "";
 			item_expiry 		= "";
 
-
-			div_all_data = "<span class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></span>"
+			div_all_data = "<div class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_batch_no='"+item_batch_no+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_gst='"+item_gst+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></div>"
 
 			item_other_image_div = '';
 			if(item_featured=="1" && item_quantity!="0"){

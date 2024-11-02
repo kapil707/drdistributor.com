48b53812cbfc52f0727cc6e62a1b65d58842981f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-02 13:12:52 +0530

    add

--- application/controllers/my_api/Api46.php
+++ application/controllers/my_api/Api46.php
@@ -1125,7 +1125,7 @@ class Api46 extends CI_Controller {
 		{
 			if($item_page_type=="medicine_category")
 			{
-				$result = $this->MedicineCategoryModel->medicine_category_api($session_yes_no,$user_nrx,$item_code,$get_record);
+				$result = $this->MedicineCategoryModel->medicine_category_api($session_yes_no,$user_nrx,$item_code,0,$get_record);
 				$items  = $result["items"];
 				$title  = $result["title"];
 				$get_record  = $result["get_record"];

5b20d4d914110eae2753a92941a33f1197df7b59
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-02 13:31:24 +0530

    add

--- application/models/model-drdistributor/medicine_category/MedicineCategoryModel.php
+++ application/models/model-drdistributor/medicine_category/MedicineCategoryModel.php
@@ -45,7 +45,7 @@ class MedicineCategoryModel extends CI_Model
 		if($order_by_type=="RAND"){
 			$this->db->order_by('id', "RAND()");
 		}else{
-			$this->db->order_by('featured', 'DESC');
+			$this->db->order_by('featured', 'asc');
         	$this->db->order_by('batchqty', 'DESC');
 		}
 		$query = $this->db->get("tbl_medicine")->result();

a615cad2aacdb19acb7e6bc2dc8b3e0b4c647810
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-03 16:33:10 +0530

    add

--- application/libraries/AppConfig.php
+++ application/libraries/AppConfig.php
@@ -8,7 +8,7 @@ class AppConfig {
     public $Weburl = "https://www.drdistributor.com/";
     public $WebJs = "10-0-0";
     public $UserProfileUrl = "https://www.drdweb.co.in/user_profile/";
-    public $MedicineImageUrl = "https://www.drdweb.co.in/user_profile/";
+    public $MedicineImageUrl = "https://www.drdweb.co.in/";
     public $ApiUrl2 = "https://www.drdweb.co.in/";
 
     public function getSiteTitle() {

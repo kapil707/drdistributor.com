dbf2341c09306e187b10a03cb5b33bf389b74f98
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-04 10:47:18 +0530

    add

--- application/views/medicine_details/medicine_details.php
+++ application/views/medicine_details/medicine_details.php
@@ -178,6 +178,6 @@ function goBack() {
 </div>
 <script>
 $(document).ready(function(){
-	get_single_medicine_info('<?= $item_code; ?>');
+	medicine_details_funcation('<?= $item_code; ?>');
 });
 </script>

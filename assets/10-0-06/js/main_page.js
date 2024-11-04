7ff91dba14587fb1febba99dd1d5ee3290541ff2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-04 16:42:35 +0530

    add

--- assets/10-0-06/js/main_page.js
+++ assets/10-0-06/js/main_page.js
@@ -258,7 +258,7 @@ function medicine_details_api(item_code)
 	if(get_page_name=="medicine_details"){
 		/*********************************** */
 		$(".top_bar_title").html("Loading....");
-		$(".main_container").hide();
+		$(".main_container").show();
 		$(".main_page_loading").show();
 		$(".main_page_no_record_found").hide();
 		$(".main_page_something_went_wrong").hide();

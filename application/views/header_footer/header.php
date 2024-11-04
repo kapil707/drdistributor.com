45181ceed067ac1d7365ca3b0fb9679b95d5e0a2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-04 11:02:15 +0530

    add

--- application/views/header_footer/header.php
+++ application/views/header_footer/header.php
@@ -608,4 +608,4 @@ var order_type = "all";
 </script>
 <script src="<?= base_url(); ?>assets/website/easyzoom/easyzoom.js"></script>
 <script src="<?= base_url(); ?>assets/website/wow_css_js/wow.js"></script>
-<script src="<?= base_url(); ?>assets/js-<?php echo $this->appconfig->getWebJs(); ?>/main_page12.js"></script>
+<script src="<?= base_url(); ?>assets/js-<?php echo $this->appconfig->getWebJs(); ?>/main_page123.js"></script>

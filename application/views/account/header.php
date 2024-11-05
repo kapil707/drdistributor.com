cb076328c331a878b2d5f2dcadaedd2fa0ab6830
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-05 10:01:28 +0530

    add

--- application/views/account/header.php
+++ application/views/account/header.php
@@ -16,27 +16,26 @@ if (isset($_COOKIE["theme_type"])) {
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link href="https://fonts.googleapis.com/css?family=Cabin:700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="<?= base_url(); ?>assets/website/css/font-awesome.min.css"> 
-	<link href="<?= base_url(); ?>assets/website/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<script src="<?= base_url(); ?>assets/website/js/jquery-2.1.4.min.js"></script>
-	<script src="<?= base_url(); ?>assets/website/js/bootstrap.min.js"></script>
-	<script src="<?= base_url(); ?>assets/website/js/bigSlide.js"></script> 
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/css/font-awesome.min.css"> 
+	<link href="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/js/jquery-2.1.4.min.js"></script>
+	<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/js/bootstrap.min.js"></script>
+	<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/js/bigSlide.js"></script> 
 
 	<?php if($theme_type=="lite") { ?>
 		<meta name="theme-color" content="#27ae60">
-		<link href="<?= base_url(); ?>assets/css/style-light.css" rel="stylesheet" type="text/css"/>
+		<link href="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/css/style-light.css" rel="stylesheet" type="text/css"/>
 	<?php } ?>
 
 	<?php if($theme_type=="dark") { ?>
 		<meta name="theme-color" content="#373d40">
-		<link href="<?= base_url(); ?>assets/css/style-dark.css" rel="stylesheet" type="text/css"/>
+		<link href="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/css/style-dark.css" rel="stylesheet" type="text/css"/>
 	<?php } ?>
 
-	<link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/css/style.css" rel="stylesheet" type="text/css" />
 
 	<link rel="icon" href="<?php echo base_url(); ?>/assets/<?php echo $this->appconfig->getWebJs(); ?>/images/logo4.png" type="image/logo" sizes="16x16">
 	
-	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/website/js/sweetalert.min.js"></script>
   </head>
-
   <body>

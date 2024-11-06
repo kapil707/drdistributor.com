292a188383f26596fdb76d2533adb15496b1a53e
kapilji <kapil707sharma@gmail.com>
2024-11-06 12:52:53 +0530

    add

--- application/controllers/Import_order.php
+++ application/controllers/Import_order.php
@@ -611,7 +611,7 @@ class Import_order extends CI_Controller {
 				$mobilenumber 	= "";
 				$modalnumber 	= "PC - Import Order";
 				$device_id    	= "";				
-				$this->MyCartModel->medicine_add_to_cart_api($UserType,$ChemistId,$SalesmanId,$order_type,$item_code,$order_quantity,$mobilenumber,$modalnumber,$device_id,$Id);
+				$this->MyCartModel->medicine_add_to_cart_api($UserType,$ChemistId,$SalesmanId,$order_type,$item_code,$order_quantity,$mobilenumber,$modalnumber,$device_id,$ItemId);
 			}
 			/******************************************/
 		}

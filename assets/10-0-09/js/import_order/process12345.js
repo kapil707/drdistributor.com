387127899aa1b003b621f29e614de48abb21abd0
kapilji <kapil707sharma@gmail.com>
2024-11-06 10:09:07 +0530

    add

--- assets/10-0-09/js/import_order/process12345.js
+++ assets/10-0-09/js/import_order/process12345.js
@@ -126,9 +126,9 @@ function MainPageFuncationCall() {
 							<div class="col-sm-12 import_order_main_${item_id}'">
 								<div class="import_order_box_left_div web-col-padding-0">
 
-									<img src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/images/featured_img.png" class="import_order_item_featured_img import_order_item_featured_${item_id}'" style="display:none">
+									<img src="'+get_base_url()+'assets/'+getWebJs()+'/images/featured_img.png" class="import_order_item_featured_img import_order_item_featured_${item_id}'" style="display:none">
 
-									<img src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/images/out_of_stock_img.png" class="import_order_item_out_of_stock_img import_order_item_out_of_stock_${item_id}'" style="display:none">
+									<img src="'+get_base_url()+'assets/'+getWebJs()+'/images/out_of_stock_img.png" class="import_order_item_out_of_stock_img import_order_item_out_of_stock_${item_id}'" style="display:none">
 
 									<img src="<?php echo base_url(); ?>/assets/<?php echo $this->appconfig->getWebJs(); ?>/images/logo4.png" width="60px;" class="all_item_image import_order_item_image_${item_id}'" alt="">
 

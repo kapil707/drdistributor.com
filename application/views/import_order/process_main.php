40237097753cf7e7dd7bb93e5d7cd457d71a6e0e
kapilji <kapil707sharma@gmail.com>
2024-11-06 13:24:17 +0530

    add

--- application/views/import_order/process_main.php
+++ application/views/import_order/process_main.php
@@ -170,5 +170,5 @@ get_page_name = "import_page";// change value taki cart pur load na ho
 order_type = "notall";// change value taki cart pur load na ho 
 </script>
 <script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/js/import_order/processmain.js"></script>
-<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/js/import_order/process_main1.js"></script>
+<script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/js/import_order/processmain1.js"></script>
 <script src="<?= base_url(); ?>assets/<?php echo $this->appconfig->getWebJs(); ?>/js/medicine_search.js"></script>

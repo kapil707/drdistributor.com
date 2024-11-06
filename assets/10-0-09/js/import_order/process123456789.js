b289f200d8ab2b2c48a785ed0e79f8b19498cc40
kapilji <kapil707sharma@gmail.com>
2024-11-06 10:18:04 +0530

    add

--- assets/10-0-09/js/import_order/process123456789.js
+++ assets/10-0-09/js/import_order/process123456789.js
@@ -84,130 +84,46 @@ function MainPageFuncationCall() {
 
 					$(".main_page_data").append(`
                         <div class="main_box_div_data import_order_main_div_${item_id} p-1">
-						<div class="row">
-							<div class="col-sm-12">
-								<div class="import_order_box_left_div web-col-padding-0">
-									(${item_id}) <?= $ChemistId;?> :
-								</div>
-								<div class="import_order_box_right_div web-col-padding-0">
-									<div class="row">
-										<div class="col-sm-9">	
-											<span class="import_order_title_1 all_item_name">
-												${item_name}
-											</span> | 
-											
-											<span class="all_item_order_quantity">Quantity : </span>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="import_order_box_left_div web-col-padding-0">
+										(${item_id}) <?= $ChemistId;?> :
+									</div>
+									<div class="import_order_box_right_div web-col-padding-0">
+										<div class="row">
+											<div class="col-sm-9">	
+												<span class="import_order_title_1 all_item_name">
+													${item_name}
+												</span> | 
+												
+												<span class="all_item_order_quantity">Quantity : </span>
 
-											<input type="text" class="import_order_hidden_item_code_${item_id}" />
+												<input type="text" class="import_order_hidden_item_code_${item_id}" />
 
-											<input type="hidden" name="" value="${item_name}" class="import_order_hidden_item_name_${item_id}" />
+												<input type="hidden" name="" value="${item_name}" class="import_order_hidden_item_name_${item_id}" />
 
-											<input type="number" name="item_qty[]" value="${item_quantity}" class="import_order_quantity_textbox_${item_id} import_order_item_order_quantity_textbox" style="width:100px;" placeholder="Eg 1,2" onchange="import_oreder_medicine_quantity_change('${item_id}')" title="Order quantity" min="1" max="1000" maxlength="4" />
+												<input type="number" name="item_qty[]" value="${item_quantity}" class="import_order_quantity_textbox_${item_id} import_order_item_order_quantity_textbox" style="width:100px;" placeholder="Eg 1,2" onchange="import_oreder_medicine_quantity_change('${item_id}')" title="Order quantity" min="1" max="1000" maxlength="4" />
 
-											<span>				
-												<a href="javascript:void(0)" onclick="import_order_medicine_delete('${item_id}')" title="Delete" class="import_order_delete_btn"><i class="fa fa-trash-o" aria-hidden="true" style="margin-right:5px;"></i> Delete</a>
-											</span>
-										</div>
-										<div class="col-sm-3 text-right">
-											<span class="all_item_mrp">
-												MRP. : 
-												<i class="fa fa-inr" aria-hidden="true"></i>
-												${item_mrp}/-
-											</span>
+												<span>				
+													<a href="javascript:void(0)" onclick="import_order_medicine_delete('${item_id}')" title="Delete" class="import_order_delete_btn"><i class="fa fa-trash-o" aria-hidden="true" style="margin-right:5px;"></i> Delete</a>
+												</span>
+											</div>
+											<div class="col-sm-3 text-right">
+												<span class="all_item_mrp">
+													MRP. : 
+													<i class="fa fa-inr" aria-hidden="true"></i>
+													${item_mrp}/-
+												</span>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
 
-							<div class="col-sm-12">
-								<div class="all_item_hr"></div>
-							</div>
-
-							<div class="col-sm-12 import_order_main_${item_id}'">
-								<div class="import_order_box_left_div web-col-padding-0">
-
-									<img src="${get_base_url()}assets/${getWebJs()}/images/featured_img.png" class="import_order_item_featured_img import_order_item_featured_${item_id}'" style="display:none">
-
-									<img src="${get_base_url()}assets/${getWebJs()}/images/out_of_stock_img.png" class="import_order_item_out_of_stock_img import_order_item_out_of_stock_${item_id}'" style="display:none">
-
-									<img src="${get_base_url()}assets/${getWebJs()}/images/logo4.png" width="60px;" class="all_item_image import_order_item_image_${item_id}'" alt="">
-
-								</div>
-
-								<div class="import_order_box_right_div web-col-padding-0">
-									<div class="row">
-										<div class="col-sm-8">
-											<span class="all_item_name import_order_item_name_${item_id}'"></span>
-
-											<span class="all_item_packing">
-												(<span class="import_order_item_packing_${item_id}'"></span> Packing) 
-											</span> - 
-
-											<span class="all_item_expiry"> 
-												Expiry : <span class="import_order_item_expiry_${item_id}'"></span>
-											</span>
-										</div>
-										<div class="col-sm-4 text-right">
-											<span class="all_item_ptr">
-												PTR : <i class="fa fa-inr" aria-hidden="true"></i> 
-												<span class="import_order_item_ptr_${item_id}'">0.00</span>/-
-											</span> | 
-
-											<span class="all_item_mrp">
-												MRP. : <i class="fa fa-inr" aria-hidden="true"></i> <span class="import_order_item_mrp_${item_id}'">0.00</span>/- 
-											</span>
-										</div>
-
-										<div class="col-sm-12">
-											<div class="all_item_hr"></div>
-										</div>
-
-										<div class="col-sm-7">
-											<span class="all_item_company">
-												By : <span class="import_order_item_company_${item_id}'"></span>
-											</span> |  
-
-											<span class="all_item_batch_no"> Batch no : 
-												<span class="import_order_item_batch_no_${item_id}'"></span>
-											</span>
-
-											<span class="all_item_scheme import_order_item_scheme_div_${item_id}'"> | 
-												<span class="import_order_item_scheme_${item_id}'"></span>
-											</span>
-										</div>
-										<div class="col-sm-5 text-right">
-											<span class="all_item_stock">
-												Stock : <span class="import_order_item_stock_${item_id}'"></span>
-											</span> | 
-
-											<span class="all_item_price">
-												*Approximate ~ : <i class="fa fa-inr" aria-hidden="true"></i> 
-												<span class="import_order_item_price_${item_id}'">0.00</span>/-
-											</span>
-										</div>
-
-										<div class="col-sm-12">
-											<div class="all_item_hr"></div>
-										</div>
-								
-										<div class="col-sm-12">
-											<span class="cart_description1 import_order_item_message_${item_id}'"> 
-											</span>
-											<span>
-												<a href="javascript:import_order_medicine_change('${item_id}')" class="import_order_edit_btn" title="Change medicine">
-													<i class="fa fa-pencil" aria-hidden="true"></i>
-													Change medicine
-												</a>
-											</span>
-											<span class="import_order_item_suggested_${item_id}'" style="display:none"> | 
-												<a href="javascript:import_order_medicine_delete_suggested('${item_id}')" title="Delete suggested" class="import_order_delete_btn"><i class="fa fa-trash-o" aria-hidden="true" style="margin-right:5px;"></i>Delete suggest</a>
-											</span>
-										</div>
-									</div>
+								<div class="col-sm-12 import_order_main_${item_id}'">
+									Loading....
 								</div>
 							</div>
 						</div>
-					</div>
 					`);
 
 					setTimeout(import_order_medicine_details(item_id),500);

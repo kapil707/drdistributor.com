bdecb9980bb9774a1138572f8f2d46e4f23f29e2
kapilji <kapil707sharma@gmail.com>
2024-11-07 14:06:21 +0530

    add

--- application/controllers/Import_order_api.php
+++ application/controllers/Import_order_api.php
@@ -179,6 +179,9 @@ class Import_order_api extends CI_Controller {
             'message' => 'Data load successfully',
             'items' => $items,
         );
+		// Send JSON response
+		header('Content-Type: application/json');
+		echo json_encode($response);
 	}
 
 	public function order_check_api(){

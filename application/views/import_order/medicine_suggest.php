f19424865d2fb693028ed88984204d2fcb8f1eb9
kapilji <kapil707sharma@gmail.com>
2024-11-07 14:16:15 +0530

    add

--- application/views/import_order/medicine_suggest.php
+++ application/views/import_order/medicine_suggest.php
@@ -51,6 +51,8 @@ $(document).ready(function(){
 	MainPageFuncationCall();
 });
 </script>
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css">
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 <script>
 var table;
 function MainPageFuncationCall() {
@@ -108,6 +110,4 @@ function delete_suggest_by_id(_id)
 	}
 }
 </script>
-<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css">
-<script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>

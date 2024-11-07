9c1da2a124494ac7f27ed86360f36fe3526dde62
kapilji <kapil707sharma@gmail.com>
2024-11-07 13:43:00 +0530

    add

--- application/views/medicine_search/medicine_search.php
+++ application/views/medicine_search/medicine_search.php
@@ -106,7 +106,7 @@ function goBack() {
 get_page_name = "medicine_search";// change value taki cart pur load na ho 
 order_type = "all";// change value taki cart pur load na ho 
 </script>
-<script src="<?php echo base_url(); ?>/assets/<?php echo $this->appconfig->getWebJs(); ?>/js/medicine_search12.js"></script>
+<script src="<?php echo base_url(); ?>/assets/<?php echo $this->appconfig->getWebJs(); ?>/js/medicine_search.js"></script>
 <script src="<?php echo base_url(); ?>/assets/<?php echo $this->appconfig->getWebJs(); ?>/js/medicine_favourite.js"></script>
 <script>
 $(document).ready(function(){	

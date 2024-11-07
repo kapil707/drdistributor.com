ee691f0a7b13cc671025567dda8b51cc58e9e399
kapilji <kapil707sharma@gmail.com>
2024-11-07 14:18:36 +0530

    add

--- application/views/import_order/medicine_suggest.php
+++ application/views/import_order/medicine_suggest.php
@@ -55,7 +55,6 @@ $(document).ready(function(){
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 
 <!-- jQuery and DataTables JS -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 </head>
 <script>

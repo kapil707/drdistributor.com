17ecfc2b7859e39d9b40ad609698cf4208751da3
kapilji <kapil707sharma@gmail.com>
2024-11-07 12:26:44 +0530

    add

--- application/libraries/AppConfig.php
+++ application/libraries/AppConfig.php
@@ -6,8 +6,8 @@ class AppConfig {
     public $siteTitle = "D.R. Distributors Pvt Ltd";
     public $siteTitle2 = "D.R. Distributors Pvt Ltd";
     public $FooterTitle = "D.R. Distributors Pvt Ltd";
-    public $WebsiteVersion = "10.0.01";
-    public $WebJs = "10-0-01";
+    public $WebsiteVersion = "10.0.02";
+    public $WebJs = "10-0-02";
     public $Weburl = "https://www.drdistributor.com/";
     public $UserProfileUrl = "https://www.drdweb.co.in/user_profile/";
     public $MedicineImageUrl = "https://www.drdweb.co.in/";

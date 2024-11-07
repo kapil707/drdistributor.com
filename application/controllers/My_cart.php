fb6160097bfce6ea67166ae5ce2e2776e5ab04a5
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-11-07 09:50:59 +0530

    add

--- application/controllers/My_cart.php
+++ application/controllers/My_cart.php
@@ -101,7 +101,7 @@ class My_cart extends CI_Controller {
 		
 		$order_type = $_POST["order_type"];
 		$items = $items_other = "";
-		if(!empty($UserType) && !empty($ChemistId) && !empty($order_type))	{
+		if(!empty($UserType) && !empty($ChemistId))	{
 
 			$result = $this->MyCartModel->my_cart_api($UserType,$ChemistId,$UserPassword,$SalesmanId,$order_type);
 

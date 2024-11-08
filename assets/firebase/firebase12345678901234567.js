76ce8c1fae55b5865733386a07f6488d7e1cd83c
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-08 14:05:10 +0530

    add

--- assets/firebase/firebase12345678901234567.js
+++ assets/firebase/firebase12345678901234567.js
@@ -22,7 +22,7 @@ const messaging = getMessaging(app);
 
 // Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./assets/firebase/sw1234567890123456.js").then(registration => {
+    navigator.serviceWorker.register("./assets/firebase/sw12345678901234567.js").then(registration => {
         getToken(messaging, {
             serviceWorkerRegistration: registration,
             vapidKey: 'BMK6vJfyFd7fqTP-reghCOTCu4DIFcDzWth46bDnvBH0teZujhO9aFsGwpvzhbSriPyu6c9GDgiZeJtVSKiGMAM'}).then((currentToken) => {
@@ -50,7 +50,7 @@ if ('serviceWorker' in navigator) {
             $('.broadcast_title').text(event.data.title);
 			$('.broadcast_message').text(event.data.message);
             if(event.data.image!=""){
-                $('.broadcast_image').text("<img src='"+event.data.image+"' width='100%'>");
+                $('.broadcast_image').html("<img src='"+event.data.image+"' width='100%'>");
             }
         }
     });

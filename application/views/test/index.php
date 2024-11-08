83e165333dcca7e355d9d487931d550c7dd66fc3
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-08 11:10:35 +0530

    add

--- application/views/test/index.php
+++ application/views/test/index.php
@@ -6,9 +6,15 @@
 </head>
 <body>
   <h1>Firebase Cloud Messaging</h1>
-  <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js"></script>
-<script>
+  <script type="module">
+        // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBNjq5AvHOoF2FqzU8FJOwNxS_BHYbUoGk",
     authDomain: "drd-noti-fire-base.firebaseapp.com",
@@ -19,54 +25,28 @@
     appId: "1:504935735685:web:ff5af385947c4cecf5e4ec",
     measurementId: "G-Z75WL6TX4Q"
   };
-  firebase.initializeApp(firebaseConfig);
-    const messaging=firebase.messaging();
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const messaging = getMessaging(app);
 
-    function IntitalizeFireBaseMessaging() {
-        messaging
-            .requestPermission()
-            .then(function () {
-                console.log("Notification Permission");
-                return messaging.getToken();
-            })
-            .then(function (token) {
-                console.log("Token : "+token);
-
-             //end code to save token into database
-                //document.getElementById("token").innerHTML=token;
-            })
-            .catch(function (reason) {
-                console.log(reason);
-            });
-    }
-
-    messaging.onMessage(function (payload) {
-        console.log(payload);
-        const notificationOption={
-            body:payload.notification.body,
-            icon:payload.notification.icon
-        };
-
-        if(Notification.permission==="granted"){
-            var notification=new Notification(payload.notification.title,notificationOption);
-
-            notification.onclick=function (ev) {
-                ev.preventDefault();
-                window.open(payload.notification.click_action,'_blank');
-                notification.close();
-            }
-        }
-
-    });
-    messaging.onTokenRefresh(function () {
-        messaging.getToken()
-            .then(function (newtoken) {
-                console.log("New Token : "+ newtoken);
-            })
-            .catch(function (reason) {
-                console.log(reason);
-				//alert(reason);
-            })
-    })
-    IntitalizeFireBaseMessaging();
-</script>
+      navigator.serviceWorker.register("<?php echo base_url(); ?>firbase_message/sw.js").then(registration => {
+          getToken(messaging, {
+              serviceWorkerRegistration: registration,
+              vapidKey: 'BMK6vJfyFd7fqTP-reghCOTCu4DIFcDzWth46bDnvBH0teZujhO9aFsGwpvzhbSriPyu6c9GDgiZeJtVSKiGMAM' }).then((currentToken) => {
+              if (currentToken) {
+                  console.log("Token is: "+currentToken);
+                  // Send the token to your server and update the UI if necessary
+                  // ...
+              } else {
+                  // Show permission request UI
+                  console.log('No registration token available. Request permission to generate one.');
+                  // ...
+              }
+          }).catch((err) => {
+              console.log('An error occurred while retrieving token. ', err);
+              // ...
+          });
+      });
+  </script>
+</body>
+</html>

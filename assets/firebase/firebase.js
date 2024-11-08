8eee917bf9cf2c1396a251d046f76530287ad9ce
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-08 12:40:51 +0530

    add

--- assets/firebase/firebase.js
+++ assets/firebase/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-navigator.serviceWorker.register("./sw.js").then(registration => {
+navigator.serviceWorker.register("./assets/firebase/sw.js").then(registration => {
     getToken(messaging, {
         serviceWorkerRegistration: registration,
         vapidKey: 'BMK6vJfyFd7fqTP-reghCOTCu4DIFcDzWth46bDnvBH0teZujhO9aFsGwpvzhbSriPyu6c9GDgiZeJtVSKiGMAM'}).then((currentToken) => {

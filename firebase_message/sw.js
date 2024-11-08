7985288b912e7b7a6f5c06a86e3d6080a24cdc56
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-08 12:23:00 +0530

    add

--- firebase_message/sw.js
+++ firebase_message/sw.js
@@ -1,21 +1,32 @@
-// Firebase Messaging Service Worker
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-self.addEventListener("push", (event) => {
+const firebaseConfig = {
+    apiKey: "AIzaSyBNjq5AvHOoF2FqzU8FJOwNxS_BHYbUoGk",
+    authDomain: "drd-noti-fire-base.firebaseapp.com",
+    databaseURL: "https://drd-noti-fire-base.firebaseio.com",
+    projectId: "drd-noti-fire-base",
+    storageBucket: "drd-noti-fire-base.firebasestorage.app",
+    messagingSenderId: "504935735685",
+    appId: "1:504935735685:web:ff5af385947c4cecf5e4ec",
+    measurementId: "G-Z75WL6TX4Q"
+};
 
-    const notif = event.data.json().notification;
+// Firebase initialize karna
+firebase.initializeApp(firebaseConfig);
 
-    event.waitUntil(self.registration.showNotification(notif.title , {
-        body: notif.body,
-        icon: notif.image,
-        data: {
-            url: notif.click_action
-        }
-    }));
+// Firebase messaging initialize karna
+const messaging = firebase.messaging();
 
-});
+// Background message handler
+messaging.onBackgroundMessage((payload) => {
+    console.log("Received background message ", payload);
 
-self.addEventListener("notificationclick", (event) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: "/firebase-logo.png" // Customize this with your icon
+    };
 
-    event.waitUntil(clients.openWindow(event.notification.data.url));
-
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });

0accb09422074bf70a448483d3d17d4efcdeb311
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-09 16:19:26 +0530

    add

--- assets/11-0-05/js/medicine_search1.js
+++ assets/11-0-05/js/medicine_search1.js
@@ -245,11 +245,10 @@ function medicine_search_api() {
 							item_description1 	= item.item_description;
 							similar_items 		= item.similar_items;
 
-
 							item_batch_no		= "xxxxxx";
 							item_gst 			= "0";
 
-							div_all_data = "<div class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_batch_no='"+item_batch_no+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_gst='"+item_gst+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' similar_items='"+similar_items+"'></div>"
+							div_all_data = "<div class='medicine_details_all_data_"+item_code+"' item_image='"+item_image+"' item_name='"+item_name+"' item_packing='"+item_packing+"' item_batch_no='"+item_batch_no+"' item_expiry='"+item_expiry+"' item_company='"+item_company+"' item_quantity='"+item_quantity+"' item_stock='"+item_stock+"' item_ptr='"+item_ptr+"' item_mrp='"+item_mrp+"' item_price='"+item_price+"' item_gst='"+item_gst+"' item_scheme='"+item_scheme+"' item_margin='"+item_margin+"' item_featured='"+item_featured+"' item_description1='"+item_description1+"' item_description2='' similar_items='"+similar_items+"'></div>"
 							
 							item_scheme_div = "";
 							if(item_scheme!="0+0")

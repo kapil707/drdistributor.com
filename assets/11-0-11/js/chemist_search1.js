cadee81d398ed7db0239e4b8ea5ff97ba67be363
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-11 11:16:46 +0530

    add

--- assets/11-0-11/js/chemist_search1.js
+++ assets/11-0-11/js/chemist_search1.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 		}
 	});
 
-    $(".medicine_search_textbox").keydown(function(e) {
+    $(".chemist_search_textbox").keydown(function(e) {
     	let listItems = $(".search_result_div ul li");
 		console.log(currentFocus + " " + listItems.length)
         if (e.key === "ArrowDown") {

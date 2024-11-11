6311a45fae28828618705b15154eb30082dd4a46
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-11 09:22:24 +0530

    add

--- application/views/header_footer/medicine_details_model.php
+++ application/views/header_footer/medicine_details_model.php
@@ -126,7 +126,11 @@
 										<input type="hidden" class="medicine_details_item_quantity_max">
 									</div>
 
-									<div class="col-sm-8 col-8">
+									<div class="col-sm-1 col-1">
+										<button type="submit" class="btn btn-danger medicine_details_item_delete main_theme_button_delete" onclick="delete_medicine('<?= $item_code; ?>')" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+									</div>
+
+									<div class="col-sm-7 col-7">
 										<button type="submit" class="btn btn-primary main_theme_button medicine_details_item_add_to_cart_btn"  onclick="medicine_add_to_cart_api()" title="Add to cart">Add to cart</button>
 
 										<button type="submit" class="btn btn-primary main_theme_button_disable medicine_details_item_add_to_cart_btn_disable" onclick="" title="Add to cart">Add to cart</button>

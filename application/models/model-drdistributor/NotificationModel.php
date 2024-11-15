b5b83d0c99d347e5d81a96b3269800c4727b07fa
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-15 13:12:18 +0530

    add

--- application/models/model-drdistributor/NotificationModel.php
+++ application/models/model-drdistributor/NotificationModel.php
@@ -66,10 +66,10 @@ class NotificationModel extends CI_Model
 				
 				$row1  =  $this->db->query("select image from tbl_division_wise where compcode='$itemid'")->row();
 				if($row1->image!=""){
-					//$image =   base_url()."uploads/manage_division_wise/photo/resize/".$row1->image;
+					//$image =   base_url()."uploads/manage_division/photo/resize/".$row1->image;
 					
 					// jab tak old server on ha to iss code say img aya ge notication m
-					$image =   constant('main_site')."uploads/manage_division_wise/photo/resize/".$row1->image;
+					$image =   constant('main_site')."uploads/manage_division/photo/resize/".$row1->image;
 				}
 				else{
 					$image = constant('main_site')."uploads/manage_users/photo/photo_1562659909.png";

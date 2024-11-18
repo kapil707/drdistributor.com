d5aaedd31191f5839ad45d555e0d15413e5471a6
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-18 15:18:04 +0530

    add

--- application/models/model-drdistributor/my_order/MyOrderModel.php
+++ application/models/model-drdistributor/my_order/MyOrderModel.php
@@ -76,23 +76,17 @@ class MyOrderModel extends CI_Model
 		$title = $download_url = "";
 
 		$this->db->select("*,m.packing,m.expiry,m.company_full_name,m.packing,m.salescm1,m.salescm2,m.image1");
+		if($UserType=="sales")
+		{
+			$this->db->where('o.selesman_id',$SalesmanId);
+		}
+		$this->db->where('o.chemist_id',$ChemistId);
 		$this->db->where('o.order_id',$ItemId);
 		$this->db->where('o.status',1);
 		$this->db->order_by('o.id','desc');
-		$this->db->from('tbl_cart as o');
+		$this->db->from('tbl_cartxxx as o');
 		$this->db->join('tbl_medicine as m', 'm.i_code = o.i_code', 'left');
 		$query = $this->db->get()->result();
-		if($UserType=="sales")
-		{
-			$this->db->select("o.*,m.packing,m.expiry,m.company_full_name,m.packing,m.salescm1,m.salescm2,m.image1");
-			$this->db->where('o.selesman_id',$SalesmanId);
-			$this->db->where('o.chemist_id',$ChemistId);
-			$this->db->where('o.order_id',$ItemId);
-			$this->db->order_by('o.id','desc');
-			$this->db->from('tbl_order as o');
-			$this->db->join('tbl_medicine as m', 'm.i_code = o.i_code', 'left');
-			$query = $this->db->get()->result();
-		}
 		foreach($query as $row)
 		{
 			if(empty($row->gstvno))
